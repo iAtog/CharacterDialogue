9f2338a85e5173f203d8b7d8b7e5d82eaff8092e
iAtog <atogmc58vani@gmail.com>
2021-08-30 10:48:56 -0400

    added method to register dialog methods for third party plugins

--- src/me/iatog/conditionaldialog/ConditionalDialogPlugin.java
+++ src/me/iatog/conditionaldialog/ConditionalDialogPlugin.java
@@ -1,7 +1,10 @@
 package me.iatog.conditionaldialog;
 
+import java.util.Map;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
+import me.iatog.conditionaldialog.dialogs.DialogMethod;
 import me.iatog.conditionaldialog.interfaces.FileFactory;
 import me.iatog.conditionaldialog.libraries.Cache;
 import me.iatog.conditionaldialog.loader.plugin.PluginLoader;
@@ -12,8 +15,11 @@ public class ConditionalDialogPlugin extends JavaPlugin {
 	private FileFactory fileFactory;
 	private Cache cache;
 	
+	private static ConditionalDialogPlugin instance;
+	
 	@Override
 	public void onEnable() {
+		instance = this;
 		this.loader = new PluginLoader(this);
 		this.cache = new Cache();
 		
@@ -35,6 +41,25 @@ public class ConditionalDialogPlugin extends JavaPlugin {
 	
 	public void setDefaultFileFactory(FileFactory factory) {
 		this.fileFactory = factory;
+	}
+	
+	public void registerMethods(DialogMethod... methods) {
+		Map<String, DialogMethod> mapMethods = getCache().getMethods();
+		for(DialogMethod method : methods) {
+			if(!mapMethods.containsKey(method.getID())) {
+				mapMethods.put(method.getID(), method);
+			}
+		}
+	}
+	
+	/**
+	 * I only use this method for third party plugins, I do not use this method and even less abuse it.
+     * <br><br>
+     * Get the main class of the ConditionalDialog.
+	 * @return the plugin main class
+	 */
+	public static ConditionalDialogPlugin getInstance() {
+		return instance;
 	}
 	
 }
