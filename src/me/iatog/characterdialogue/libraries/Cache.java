1b4789d7b78134ef1e0abb26d902e64f00b026b9
iAtog <atogmc58vani@gmail.com>
2021-08-31 08:44:52 -0400

    added sessions to cache

--- src/me/iatog/characterdialogue/libraries/Cache.java
+++ src/me/iatog/characterdialogue/libraries/Cache.java
@@ -2,19 +2,27 @@ package me.iatog.characterdialogue.libraries;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 import me.iatog.characterdialogue.dialogs.DialogMethod;
+import me.iatog.characterdialogue.session.DialogSession;
 
 public class Cache {
 	
 	private Map<String, DialogMethod> methods;
+	private Map<UUID, DialogSession> sessions;
 	
 	public Cache() {
 		this.methods = new HashMap<>();
+		this.sessions = new HashMap<>();
 	}
 	
 	public Map<String, DialogMethod> getMethods() {
 		return methods;
+	}
+	
+	public Map<UUID, DialogSession> getSessions() {
+		return sessions;
 	}
 		
 	public void clearAll() {
