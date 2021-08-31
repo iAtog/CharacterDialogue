bac3623ac0349180ea7dfd29c664a7b98278dd4d
iAtog <atogmc58vani@gmail.com>
2021-08-31 10:20:34 -0400

    remastered wait method

--- src/me/iatog/characterdialogue/dialogs/method/WaitMethod.java
+++ src/me/iatog/characterdialogue/dialogs/method/WaitMethod.java
@@ -1,25 +1,37 @@
 package me.iatog.characterdialogue.dialogs.method;
 	
-import java.util.concurrent.TimeUnit;
-
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
+import me.iatog.characterdialogue.CharacterDialogPlugin;
 import me.iatog.characterdialogue.dialogs.DialogMethod;
+import me.iatog.characterdialogue.session.DialogSession;
 
 public class WaitMethod extends DialogMethod {
-
-	public WaitMethod() {
+	
+	private CharacterDialogPlugin main;
+	
+	public WaitMethod(CharacterDialogPlugin main) {
 		super("wait");
+		this.main = main;
 	}
 
 	@Override
-	public void cast(Player player, String arg) {
+	public void execute(Player player, String arg, DialogSession session) {
 		long seconds = Long.valueOf(arg);
-		try {
-		    TimeUnit.SECONDS.sleep(seconds);
-		    System.out.println(player.getName()+": waited "+seconds+" seconds");
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+		int next = session.getCurrentIndex() + 1;
+		session.cancel();
+		Bukkit.getScheduler().runTaskLater(main, new Runnable() {
+
+			@Override
+			public void run() {
+				if(next < session.getDialogs().size()) {
+					session.start(next);
+					System.out.println(player.getName()+": waited "+seconds+" seconds");
+				}
+			}
+			
+		}, seconds*20);
+		
 	}
 }
