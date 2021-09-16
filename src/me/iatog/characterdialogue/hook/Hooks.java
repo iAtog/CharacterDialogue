0d4596a1377bda82023e4afc7389eb0131cf5e65
Atog <Atog@192.168.1.104>
2021-09-15 22:36:53 -0400

    removed holographic displays hook

--- src/me/iatog/characterdialogue/hook/Hooks.java
+++ src/me/iatog/characterdialogue/hook/Hooks.java
@@ -2,30 +2,20 @@ package me.iatog.characterdialogue.hook;
 
 import org.bukkit.Bukkit;
 
-import me.iatog.characterdialogue.hook.holodisplays.HolographicDisplaysHook;
 import me.iatog.characterdialogue.hook.papi.PAPIHook;
 
 public class Hooks {
 	
 	private PAPIHook papiHook;
-	private HolographicDisplaysHook holoHook;
 	
 	public Hooks() {
 		if(isPlaceHolderAPIEnabled()) {
 			this.papiHook = new PAPIHook();
 		}
-		
-		if(this.isHoloDisplaysEnabled()) {
-			this.holoHook = new HolographicDisplaysHook();
-		}
 	}
 	
 	public PAPIHook getPlaceHolderAPIHook() {
 		return papiHook;
-	}
-	
-	public HolographicDisplaysHook getHoloDisplaysHook() {
-		return holoHook;
 	}
 	
 	public boolean isPlaceHolderAPIEnabled() {
--- src/me/iatog/characterdialogue/hook/holodisplays/HolographicDisplaysHook.java
+++
@@ -1,5 +0,0 @@
-package me.iatog.characterdialogue.hook.holodisplays;
-
-public class HolographicDisplaysHook {
-	
-}
