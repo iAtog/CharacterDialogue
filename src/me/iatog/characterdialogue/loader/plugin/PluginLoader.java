9e810de65c5226b998baf508852448003a929a63
iAtog <atogmc58vani@gmail.com>
2021-09-16 14:57:25 -0400

    added unload handling

--- src/me/iatog/characterdialogue/loader/plugin/PluginLoader.java
+++ src/me/iatog/characterdialogue/loader/plugin/PluginLoader.java
@@ -1,21 +1,23 @@
 package me.iatog.characterdialogue.loader.plugin;
 
-import org.bukkit.Bukkit;
+import java.util.ArrayList;
+import java.util.List;
 
 import me.iatog.characterdialogue.CharacterDialoguePlugin;
 import me.iatog.characterdialogue.loader.Loader;
 import me.iatog.characterdialogue.loader.cache.CacheLoader;
 import me.iatog.characterdialogue.loader.command.CommandLoader;
 import me.iatog.characterdialogue.loader.file.FileLoader;
-import me.iatog.characterdialogue.loader.hologram.HologramLoader;
 import me.iatog.characterdialogue.loader.listener.ListenerLoader;
 
 public class PluginLoader implements Loader {
 	
 	private CharacterDialoguePlugin main;
+	private List<Loader> loaders;
 	
 	public PluginLoader(CharacterDialoguePlugin main) {
 		this.main = main;
+		this.loaders = new ArrayList<>();
 	}
 	
 	@Override
@@ -26,16 +28,30 @@ public class PluginLoader implements Loader {
 				new CacheLoader(main),
 				new CommandLoader(main)
 				);
+		/*
 		if(Bukkit.getPluginManager().isPluginEnabled("HolographicDisplays")) {
 			loadLoaders(new HologramLoader(main));
 		}
-		
+		*/
 		main.getLogger().info("§a"+main.getDescription().getName()+" loaded. §7"+main.getDescription().getVersion());
 	}
-		
+	
+	@Override
+	public void unload() {
+		loaders.forEach(loader -> {
+			loader.unload();
+		});
+		loaders.clear();
+	}
+	
+	public List<Loader> getLoaders() {
+		return loaders;
+	}
+	
 	private void loadLoaders(Loader...loaders) {
 		for(Loader loader : loaders) {
 			loader.load();
+			this.loaders.add(loader);
 		}
 	}
 	
