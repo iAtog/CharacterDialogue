0a62384e28675f1b51c1e6a6f24d8bddcfaa05c8
iAtog <atogmc58vani@gmail.com>
2021-10-14 19:44:58 -0400

    added after first time function

--- src/me/iatog/characterdialogue/listeners/NPCInteractListener.java
+++ src/me/iatog/characterdialogue/listeners/NPCInteractListener.java
@@ -1,7 +1,9 @@
 package me.iatog.characterdialogue.listeners;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.logging.Level;
 
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
@@ -13,7 +15,6 @@ import me.iatog.characterdialogue.api.CharacterDialogueAPI;
 import me.iatog.characterdialogue.enums.ClickType;
 import me.iatog.characterdialogue.interfaces.FileFactory;
 import me.iatog.characterdialogue.libraries.YamlFile;
-import me.iatog.characterdialogue.session.DialogSession;
 import net.citizensnpcs.api.event.NPCClickEvent;
 import net.citizensnpcs.api.event.NPCLeftClickEvent;
 import net.citizensnpcs.api.event.NPCRightClickEvent;
@@ -43,28 +44,55 @@ public class NPCInteractListener implements Listener {
 		int id = npc.getId();
 		FileFactory files = main.getFileFactory();
 		YamlFile dialogsFile = files.getDialogs();
+		YamlFile playerCache = main.getFileFactory().getPlayerCache();
 		CharacterDialogueAPI api = main.getApi();
 		Optional<String> search = api.searchDialogueByNPCId(id);
+		String path = "players." + player.getUniqueId();
 		
-		if(!search.isPresent()) {
-			return;
-		}
-		
-		if(main.getCache().getSessions().containsKey(player.getUniqueId())) {
+		if(!search.isPresent() || main.getCache().getSessions().containsKey(player.getUniqueId())) {
 			return;
 		}
 		
 		ConfigurationSection dialogueSection = dialogsFile.getConfigurationSection(search.get());
-		
 		ClickType clickType = ClickType.valueOf(dialogueSection.getString("click"));
+
 		if(((event instanceof NPCRightClickEvent && clickType != ClickType.RIGHT) || (event instanceof NPCLeftClickEvent && clickType != ClickType.LEFT)) && clickType != ClickType.ALL) {
 			return;
 		}
 		
+		if(dialogueSection.getBoolean("after-first-time.enabled", false)) {
+			List<String> readedDialogues = playerCache.getStringList(path + ".readed-dialogues");
+			List<String> afterFirstTimeDialog = dialogueSection.getStringList("after-first-time.dialog");
+						
+			if(afterFirstTimeDialog != null && playerCache.contains(path) && readedDialogues.contains(dialogueSection.getName())) {
+				main.getApi().executeDialog(afterFirstTimeDialog, player, clickType, id, dialogueSection.getString("display-name", "John the NPC"));
+				return;
+			}
+		}
+		
 		List<String> dialogs = dialogueSection.getStringList("dialog");
 		
+		main.getApi().executeDialog(dialogs, player, clickType, id, dialogueSection.getString("display-name", "John the NPC"));
+		
+		if(dialogueSection.getBoolean("after-first-time.enabled", false)) {
+			List<String> readedDialogues = playerCache.getStringList(path + ".readed-dialogues");
+			
+			if(!playerCache.contains(path)) {
+				readedDialogues = new ArrayList<>();
+			}
+			
+			if(readedDialogues.contains(dialogueSection.getName())) {
+				return;
+			}
+			
+			readedDialogues.add(dialogueSection.getName());
+			playerCache.set(path + ".readed-dialogues", readedDialogues);
+			playerCache.save();
+			main.getLogger().log(Level.FINE, "Saved cache for " + player.getName() + " in " + dialogueSection.getName());
+		}
+		/*
 		DialogSession session = new DialogSession(main, player, dialogs, clickType, id, dialogueSection.getString("display-name", "John the NPC"));
 		main.getCache().getSessions().put(player.getUniqueId(), session);
-		session.start(0);
+		session.start(0);*/
 	}
 }
