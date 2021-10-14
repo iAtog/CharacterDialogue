03bc7c1ea8f98f80b41ad039f21f99a89f7c2017
iAtog <atogmc58vani@gmail.com>
2021-10-14 19:42:47 -0400

    added permissions for main command and add 'clear-cache' command

--- src/me/iatog/characterdialogue/command/CharacterDialogueCommand.java
+++ src/me/iatog/characterdialogue/command/CharacterDialogueCommand.java
@@ -2,19 +2,25 @@ package me.iatog.characterdialogue.command;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import me.fixeddev.commandflow.annotated.CommandClass;
 import me.fixeddev.commandflow.annotated.annotation.Command;
+import me.fixeddev.commandflow.annotated.annotation.OptArg;
 import me.iatog.characterdialogue.CharacterDialoguePlugin;
 import me.iatog.characterdialogue.libraries.YamlFile;
+import me.iatog.characterdialogue.session.DialogSession;
 
 @Command(names = {
 		"characterdialogue", "cdp", "characterd", "ddialogue"
-})
+},      permission = "characterdialogue.use",
+		desc = "CharacterDialogue main command")
 public class CharacterDialogueCommand implements CommandClass {
 	
 	private CharacterDialoguePlugin main;
@@ -30,7 +36,9 @@ public class CharacterDialogueCommand implements CommandClass {
 		sender.sendMessage(translateList(language.getStringList("help-message")).stream().toArray(String[]::new));
 	}
 	
-	@Command(names = "reload", desc = "Reload the plugin")
+	@Command(names = "reload",
+			permission = "characterdialogue.reload",
+			desc = "Reload the plugin")
 	public void reloadCommand(CommandSender sender) {
 		main.getFileFactory().reload();
 		if(Bukkit.getPluginManager().isPluginEnabled("HolographicDisplays")) {
@@ -38,6 +46,32 @@ public class CharacterDialogueCommand implements CommandClass {
 		}
 		
 		sender.sendMessage(ChatColor.translateAlternateColorCodes('&', language.getString("reload-message")));
+	}
+	
+	@Command(names = "clear-cache",
+	         permission = "characterdialogue.clear-cache",
+	         desc = "Clear a player memory cache")
+	public void clearCache(CommandSender sender, @OptArg("$") String arg) {
+		if(arg.equals("$")) {
+			sender.sendMessage("§cYou need to specify a player name.");
+			return;
+		}
+		
+		Player target = Bukkit.getPlayer(arg);
+		if(target == null || !target.isOnline()) {
+			sender.sendMessage("§cThe player '" + arg + "' isn't online.");
+			return;
+		}
+		
+		Map<UUID, DialogSession> cache = main.getCache().getSessions();
+		
+		if(cache.containsKey(target.getUniqueId())) {
+			DialogSession session = cache.remove(target.getUniqueId());
+			session.cancel();
+			sender.sendMessage("§aCleared cache for " + arg + " in "+session.getDisplayName() + " §r§adialog.");
+		} else {
+			sender.sendMessage("§cThat player doesn't have any data in the memory cache.");
+		}
 	}
 	
 	private List<String> translateList(List<String> list) {
