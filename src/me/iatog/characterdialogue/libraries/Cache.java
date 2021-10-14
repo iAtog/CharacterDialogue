84202086c71bb9da89ad22424bd9347e1ad35bb4
iAtog <atogmc58vani@gmail.com>
2021-10-14 19:41:40 -0400

    Removed from cache "PlayerCache" object

--- src/me/iatog/characterdialogue/libraries/Cache.java
+++ src/me/iatog/characterdialogue/libraries/Cache.java
@@ -5,19 +5,16 @@ import java.util.Map;
 import java.util.UUID;
 
 import me.iatog.characterdialogue.dialogs.DialogMethod;
-import me.iatog.characterdialogue.misc.PlayerCache;
 import me.iatog.characterdialogue.session.DialogSession;
 
 public class Cache {
 
 	private Map<String, DialogMethod> methods;
 	private Map<UUID, DialogSession> sessions;
-	private Map<UUID, PlayerCache> playerCache;
 
 	public Cache() {
 		this.methods = new HashMap<>();
 		this.sessions = new HashMap<>();
-		this.playerCache = new HashMap<>();
 	}
 
 	public Map<String, DialogMethod> getMethods() {
@@ -28,14 +25,9 @@ public class Cache {
 		return sessions;
 	}
 
-	public Map<UUID, PlayerCache> getPlayerCache() {
-		return playerCache;
-	}
-
 	public void clearAll() {
 		methods.clear();
 		sessions.clear();
-		playerCache.clear();
 	}
 
 }
