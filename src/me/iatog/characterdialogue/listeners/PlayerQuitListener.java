ab7026d322aaf4d656327fbd3497cc364913c4a0
iAtog <atogmc58vani@gmail.com>
2021-10-15 15:42:25 -0400

    added event for manage the session when player quit

---
+++ src/me/iatog/characterdialogue/listeners/PlayerQuitListener.java
@@ -0,0 +1,35 @@
+package me.iatog.characterdialogue.listeners;
+
+import java.util.Map;
+import java.util.UUID;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+import me.iatog.characterdialogue.CharacterDialoguePlugin;
+import me.iatog.characterdialogue.session.DialogSession;
+
+public class PlayerQuitListener implements Listener {
+	
+	private CharacterDialoguePlugin main;
+	
+	public PlayerQuitListener(CharacterDialoguePlugin main) {
+		this.main = main;
+	}
+	
+	@EventHandler
+	public void onQuit(PlayerQuitEvent event) {
+		Player player = event.getPlayer();
+		Map<UUID, DialogSession> cache = main.getCache().getSessions();
+		
+		if(!cache.containsKey(player.getUniqueId())) {
+			return;
+		}
+		
+		DialogSession session = cache.remove(player.getUniqueId());
+		session.cancel();
+	}
+	
+}
