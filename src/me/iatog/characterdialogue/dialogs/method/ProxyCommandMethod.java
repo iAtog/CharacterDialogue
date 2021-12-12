3b361674f4d6f3754116fec11e0a22b183a5be0f
iAtog <atogmc58vani@gmail.com>
2021-12-12 16:29:21 -0400

    improved code

--- src/me/iatog/characterdialogue/dialogs/method/ProxyCommandMethod.java
+++ src/me/iatog/characterdialogue/dialogs/method/ProxyCommandMethod.java
@@ -22,18 +22,14 @@ public class ProxyCommandMethod extends DialogMethod {
 	public void execute(Player player, String arg, DialogSession session) {
 		ByteArrayDataOutput out = ByteStreams.newDataOutput();
 		String[] args = arg.split(",");
-		String command = null;
-		String channel = null;
-
-		if(args.length == 1) {
-			command = args[0];
-			channel = "BungeeCord";
-		} else if(args.length == 2) {
-			command = args[0];
-			channel = args[1];
-		} else {
+		
+		
+		if(args.length == 0) {
 			return;
 		}
+		
+		String command = args[0];
+		String channel = args.length > 1 ? args[1] : "BungeeCord";
 		
 		if(command.startsWith("/")) {
 			command = command.substring(1);
