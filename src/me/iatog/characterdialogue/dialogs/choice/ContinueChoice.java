1ce7f123aa0b07e7eff9dc027f31b062540fdbf2
iAtog <atogmc58vani@gmail.com>
2021-12-13 08:20:19 -0400

    added continue choice

---
+++ src/me/iatog/characterdialogue/dialogs/choice/ContinueChoice.java
@@ -0,0 +1,16 @@
+package me.iatog.characterdialogue.dialogs.choice;
+
+import me.iatog.characterdialogue.dialogs.DialogChoice;
+import me.iatog.characterdialogue.session.DialogSession;
+
+public class ContinueChoice extends DialogChoice {
+
+	public ContinueChoice() {
+		super("continue");
+	}
+
+	@Override
+	public void onSelect(DialogSession session) {
+		session.start(session.getCurrentIndex() + 1);
+	}
+}
