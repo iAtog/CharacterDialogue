02feaecc8bd81d2f128bca654585a06d81262b78
iAtog <atogmc58vani@gmail.com>
2021-12-13 08:20:33 -0400

    added destroy dialog session choice

---
+++ src/me/iatog/characterdialogue/dialogs/choice/DestroyChoice.java
@@ -0,0 +1,19 @@
+package me.iatog.characterdialogue.dialogs.choice;
+
+import org.bukkit.entity.Player;
+
+import me.iatog.characterdialogue.dialogs.DialogChoice;
+import me.iatog.characterdialogue.session.DialogSession;
+
+public class DestroyChoice extends DialogChoice {
+
+	public DestroyChoice() {
+		super("destroy");
+	}
+
+	@Override
+	public void onSelect(DialogSession session) {
+		session.destroy();
+	}
+	
+}
