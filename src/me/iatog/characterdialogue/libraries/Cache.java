01b220203e131a162846c1da6fada6477e362681
iAtog <atogmc58vani@gmail.com>
2021-12-13 21:09:53 -0400

    added choice sessions to memory cache

--- src/me/iatog/characterdialogue/libraries/Cache.java
+++ src/me/iatog/characterdialogue/libraries/Cache.java
@@ -5,29 +5,37 @@ import java.util.Map;
 import java.util.UUID;
 
 import me.iatog.characterdialogue.dialogs.DialogMethod;
+import me.iatog.characterdialogue.session.ChoiceSession;
 import me.iatog.characterdialogue.session.DialogSession;
 
 public class Cache {
 
 	private Map<String, DialogMethod> methods;
-	private Map<UUID, DialogSession> sessions;
+	private Map<UUID, DialogSession> dialogSessions;
+	private Map<UUID, ChoiceSession> choiceSessions;
 
 	public Cache() {
 		this.methods = new HashMap<>();
-		this.sessions = new HashMap<>();
+		this.dialogSessions = new HashMap<>();
+		this.choiceSessions = new HashMap<>();
 	}
 
 	public Map<String, DialogMethod> getMethods() {
 		return methods;
 	}
 
-	public Map<UUID, DialogSession> getSessions() {
-		return sessions;
+	public Map<UUID, DialogSession> getDialogSessions() {
+		return dialogSessions;
+	}
+	
+	public Map<UUID, ChoiceSession> getChoiceSessions() {
+		return choiceSessions;
 	}
 
 	public void clearAll() {
 		methods.clear();
-		sessions.clear();
+		dialogSessions.clear();
+		choiceSessions.clear();
 	}
 
 }
