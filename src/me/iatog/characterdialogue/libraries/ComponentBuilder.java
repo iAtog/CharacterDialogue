8e8431b8e57c158dfdc1b4ffffd4eafd8e5fa5fc
iAtog <atogmc58vani@gmail.com>
2021-12-14 01:13:12 -0400

    created component builder for choices

---
+++ src/me/iatog/characterdialogue/libraries/ComponentBuilder.java
@@ -0,0 +1,54 @@
+package me.iatog.characterdialogue.libraries;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
+
+/**
+ * class made for cleaner handling of chat components
+ * @author iAtog
+ */
+public class ComponentBuilder {
+	
+	List<BaseComponent> components;
+	
+	public ComponentBuilder() {
+		this.components = new ArrayList<>();
+	}
+	
+	public ComponentBuilder addComponent(BaseComponent component) {
+		this.components.add(component);
+		return this;
+	}
+	
+	public ComponentBuilder addTextComponent(String message) {
+		return addComponent(new TextComponent(message));
+	}
+	
+	public ComponentBuilder addTextComponent(String message, ChatColor color, HoverEvent hover, ClickEvent click) {
+		TextComponent component = new TextComponent(message);
+		component.setHoverEvent(hover);
+		component.setClickEvent(click);
+		
+		if(color != null) {
+			component.setColor(color);
+		}
+		
+		return addComponent(component);
+	}
+	
+	public BaseComponent[] build() {
+		return components.toArray(new BaseComponent[0]);
+	}
+	
+	public List<BaseComponent> getComponents() {
+		return Collections.unmodifiableList(components);
+	}
+	
+}
