eed0be420cc1401f017e16973388b7c4275e5cc6
iAtog <atogmc58vani@gmail.com>
2021-12-14 15:45:04 -0400

    added 'requireArgument' as boolean

--- src/me/iatog/characterdialogue/dialogs/DialogChoice.java
+++ src/me/iatog/characterdialogue/dialogs/DialogChoice.java
@@ -7,14 +7,20 @@ import me.iatog.characterdialogue.session.DialogSession;
 public abstract class DialogChoice {
 	
 	private String id;
+	private boolean requireArgument;
 	
-	public DialogChoice(@Nullable String id) {
+	public DialogChoice(String id, boolean requireArgument) {
 		this.id = id;
+		this.requireArgument = requireArgument;
 	}
 	
 	public abstract void onSelect(DialogSession session);
 	
 	public String getId() {
 		return id;
+	}
+	
+	public boolean isArgumentRequired() {
+		return requireArgument;
 	}
 }
