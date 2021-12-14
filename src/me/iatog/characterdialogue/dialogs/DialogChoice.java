97faa616b2da15a41197004d69b9ce7b6173612e
iAtog <atogmc58vani@gmail.com>
2021-12-13 21:21:54 -0400

    simplified 'DialogChoice' class

--- src/me/iatog/characterdialogue/dialogs/DialogChoice.java
+++ src/me/iatog/characterdialogue/dialogs/DialogChoice.java
@@ -1,50 +1,20 @@
 package me.iatog.characterdialogue.dialogs;
 
-import java.util.Map;
-import java.util.UUID;
-
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.player.PlayerEvent;
 import org.jetbrains.annotations.Nullable;
 
-import me.iatog.characterdialogue.CharacterDialoguePlugin;
-import me.iatog.characterdialogue.libraries.Cache;
 import me.iatog.characterdialogue.session.DialogSession;
 
-public abstract class Choice<T extends PlayerEvent> {
+public abstract class DialogChoice {
 	
 	private String id;
-	private T event;
 	
-	public Choice(@Nullable String id, @Nullable T event) {
+	public DialogChoice(@Nullable String id) {
 		this.id = id;
-		this.event = event;
 	}
 	
-	public abstract void onSelect();
-	public abstract void onEvent(T event, DialogSession session);
-	
-	@EventHandler
-	public void eventExecution(T event) {
-		Player player = event.getPlayer();
-		CharacterDialoguePlugin characterDialogue = CharacterDialoguePlugin.getInstance();
-		Cache cache = characterDialogue.getCache();
-		Map<UUID, DialogSession> sessions = cache.getSessions();
-		
-		if(!sessions.containsKey(player.getUniqueId())) {
-			return;
-		}
-		
-		onEvent(event, sessions.get(player.getUniqueId()));
-	}
+	public abstract void onSelect(DialogSession session);
 	
 	public String getId() {
 		return id;
 	}
-	
-	public T getEvent() {
-		return event;
-	}
-	
 }
