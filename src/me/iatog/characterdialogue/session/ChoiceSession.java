e44bede527001e2036fc2ea834dda973032d996c
iAtog <atogmc58vani@gmail.com>
2022-01-19 14:56:03 -0400

    updated & improved choice session

--- src/me/iatog/characterdialogue/session/ChoiceSession.java
+++ src/me/iatog/characterdialogue/session/ChoiceSession.java
@@ -4,7 +4,6 @@ import java.util.Map;
 import java.util.TreeMap;
 import java.util.UUID;
 
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 import me.iatog.characterdialogue.CharacterDialoguePlugin;
@@ -13,53 +12,60 @@ import me.iatog.characterdialogue.interfaces.Session;
 import me.iatog.characterdialogue.misc.Choice;
 
 public class ChoiceSession implements Session {
-	
+
 	private CharacterDialoguePlugin main;
-	private UUID player;
+	private Player player;
 	private UUID uuid;
 	private Map<Integer, Choice> choices;
-	
+
 	public ChoiceSession(CharacterDialoguePlugin main, Player player) {
 		this.main = main;
-		this.player = player.getUniqueId();
+		this.player = player;
 		this.uuid = UUID.randomUUID();
 		this.choices = new TreeMap<>();
 	}
 	
-	public AddChoiceResult addChoice(int index, String choice, String message) {
-		Map<String, DialogChoice> cache = main.getCache().getChoices();
+	public Choice getChoice(int index) {
+		return choices.get(index);
+	}
+	
+	public boolean addChoice(int index, String message, Class<? extends DialogChoice> clazz, String argument) {
+		//Map<String, DialogChoice> cache = main.getCache().getChoices();
 		
-		if(choices.containsKey(index) ) {
-			return AddChoiceResult.ALREADY_EXISTS;
-		} else if(!cache.containsKey(choice)) {
-			return AddChoiceResult.UNKNOWN_CHOICE;
+		if (choices.containsKey(index)) {
+			return false;
 		}
 		
-		choices.put(index, new Choice(cache.get(choice), message));
-		return AddChoiceResult.SUCCESS;
+		choices.put(index, new Choice(index, message, clazz, argument));
+		return true;
+	}
+
+	public boolean addChoice(String message, Class<? extends DialogChoice> clazz, String argument) {
+		return addChoice(choices.size() + 1, message, clazz, argument);
 	}
 	
-	public AddChoiceResult addChoice(String choice, String message) {
-		return addChoice(choices.size() + 1, choice, message);
+	public boolean addChoice(String message, Class<? extends DialogChoice> clazz) {
+		return addChoice(choices.size() + 1, message, clazz, "");
 	}
-	
+
 	public boolean check() {
 		return false;
 	}
-	
+
 	public Player getPlayer() {
-		return Bukkit.getPlayer(player);
+		return player;
 	}
-	
+
 	public Map<Integer, Choice> getChoices() {
 		return choices;
 	}
-	
+
 	public UUID getUniqueId() {
 		return uuid;
 	}
-
-	public enum AddChoiceResult {
-		ALREADY_EXISTS, UNKNOWN_CHOICE, SUCCESS;
+	
+	@Override
+	public void destroy() {
+		main.getCache().getChoiceSessions().remove(player.getUniqueId());
 	}
 }
