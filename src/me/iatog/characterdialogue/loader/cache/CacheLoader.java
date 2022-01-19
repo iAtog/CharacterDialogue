e26ddf1dcba7650199ffe25b72e9971e22fe477a
iAtog <atogmc58vani@gmail.com>
2022-01-19 15:02:08 -0400

    added choices and choice method to the registry

--- src/me/iatog/characterdialogue/loader/cache/CacheLoader.java
+++ src/me/iatog/characterdialogue/loader/cache/CacheLoader.java
@@ -3,7 +3,9 @@ package me.iatog.characterdialogue.loader.cache;
 import me.iatog.characterdialogue.CharacterDialoguePlugin;
 import me.iatog.characterdialogue.dialogs.choice.ContinueChoice;
 import me.iatog.characterdialogue.dialogs.choice.DestroyChoice;
+import me.iatog.characterdialogue.dialogs.choice.SendChoice;
 import me.iatog.characterdialogue.dialogs.method.BroadcastMethod;
+import me.iatog.characterdialogue.dialogs.method.ChoiceMethod;
 import me.iatog.characterdialogue.dialogs.method.CommandMethod;
 import me.iatog.characterdialogue.dialogs.method.DispatchCommandMethod;
 import me.iatog.characterdialogue.dialogs.method.EffectMethod;
@@ -22,6 +24,7 @@ public class CacheLoader implements Loader {
 		this.main = main;
 	}
 	
+	@SuppressWarnings("unchecked")
 	@Override
 	public void load() {
 		main.registerMethods(
@@ -33,12 +36,14 @@ public class CacheLoader implements Loader {
 				new CommandMethod(),
 				new TeleportMethod(),
 				new EffectMethod(main),
-				new SendServerMethod(main)
+				new SendServerMethod(main),
+				new ChoiceMethod(main)
 				);
 		
 		main.registerChoices(
 				new ContinueChoice(),
-				new DestroyChoice()
+				new DestroyChoice(),
+				new SendChoice()
 				);
 	}
 
