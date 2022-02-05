892729b5a919d3d511ec64a136ab48e754c6020d
iAtog <atogmc58vani@gmail.com>
2022-02-05 18:41:46 -0400

    chore: finished choice method

--- src/me/iatog/characterdialogue/dialogs/method/ChoiceMethod.java
+++ src/me/iatog/characterdialogue/dialogs/method/ChoiceMethod.java
@@ -4,6 +4,8 @@ import java.util.Map;
 import java.util.UUID;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -13,8 +15,7 @@ import me.iatog.characterdialogue.CharacterDialoguePlugin;
 import me.iatog.characterdialogue.api.events.ChoiceSelectEvent;
 import me.iatog.characterdialogue.dialogs.DialogChoice;
 import me.iatog.characterdialogue.dialogs.DialogMethod;
-import me.iatog.characterdialogue.dialogs.choice.ContinueChoice;
-import me.iatog.characterdialogue.dialogs.choice.DestroyChoice;
+import me.iatog.characterdialogue.libraries.YamlFile;
 import me.iatog.characterdialogue.misc.Choice;
 import me.iatog.characterdialogue.session.ChoiceSession;
 import me.iatog.characterdialogue.session.DialogSession;
@@ -33,28 +34,57 @@ public class ChoiceMethod extends DialogMethod<CharacterDialoguePlugin> implemen
 	@Override
 	public void execute(Player player, String arg, DialogSession session) {
 		Map<UUID, ChoiceSession> sessions = provider.getCache().getChoiceSessions();
-
+		YamlFile config = provider.getFileFactory().getConfig();
+		
+		
 		if (sessions.containsKey(player.getUniqueId())) {
 			return;
 		}
 
 		ChoiceSession choiceSession = new ChoiceSession(provider, player);
+		
+		if(!config.contains("choice.choices." + arg)) {
+			provider.getLogger().warning("The choice \"" + arg + "\" doesn't exists.");
+			return;
+		}
+		
+		for(String choice : config.getConfigurationSection("choice.choices." + arg).getKeys(false)) {
+			ConfigurationSection section = config.getConfigurationSection("choice.choices." + arg + "." + choice);
+			String type = section.getString("type");
+			String message = section.getString("message", "no message specified");
+			String argument = section.getString("argument", "");
+			
+			if(type == null || !provider.getCache().getChoices().containsKey(type)) {
+				provider.getLogger().warning("The type of choice '" + choice + "' in " + arg + " isn't valid");
+				continue;
+			}
+			
+			Class<? extends DialogChoice> choiceClass = provider.getCache().getChoices().get(type).getClass();
+			
+			choiceSession.addChoice(message, choiceClass, argument);
+		}
+		
+		/*
 		choiceSession.addChoice("My mom", DestroyChoice.class);
 		choiceSession.addChoice("My sis", ContinueChoice.class);
 		choiceSession.addChoice("My stepsis", ContinueChoice.class);
+		*/
+		
 		session.cancel();
 		sessions.put(player.getUniqueId(), choiceSession);
 		ComponentBuilder questions = new ComponentBuilder("");
-
+		String model = config.getString("choice.text-model", "&a{I})&e {S}");
+		
 		choiceSession.getChoices().forEach((index, choice) -> {
-			questions.append("§a" + index + "> §e" + choice.getMessage() + " \n")
+			String parsedModel = ChatColor.translateAlternateColorCodes('&', model)
+					.replace("{I}", String.valueOf(index))
+					.replace("{S}", choice.getMessage());
+			// "§a" + index + "> §e" + choice.getMessage()
+			questions.append(parsedModel + " \n")
 					.event(new ClickEvent(ClickEvent.Action.RUN_COMMAND,
 							"/;/select " + choiceSession.getUniqueId() + " " + index))
 					.event(new HoverEvent(HoverEvent.Action.SHOW_TEXT, getSelectText(index)));
 		});
-
-		// questions.append("\n§aSelecciona uno.").event(new
-		// ClickEvent(ClickEvent.Action.RUN_COMMAND, ""));
 
 		player.spigot().sendMessage(questions.create());
 	}
