f288e6ba5d2ae40e0b1e6e314573ca7b99d36f98
iAtog <atogmc58vani@gmail.com>
2022-02-05 19:36:39 -0400

    feat: added permissions to the dialogs

--- src/me/iatog/characterdialogue/listeners/NPCInteractListener.java
+++ src/me/iatog/characterdialogue/listeners/NPCInteractListener.java
@@ -7,7 +7,9 @@ import org.bukkit.event.Listener;
 import me.iatog.characterdialogue.CharacterDialoguePlugin;
 import me.iatog.characterdialogue.api.CharacterDialogueAPI;
 import me.iatog.characterdialogue.api.dialog.Dialogue;
+import me.iatog.characterdialogue.api.dialog.Dialogue.DialoguePermission;
 import me.iatog.characterdialogue.enums.ClickType;
+import me.iatog.characterdialogue.placeholders.Placeholders;
 import net.citizensnpcs.api.event.NPCClickEvent;
 import net.citizensnpcs.api.event.NPCLeftClickEvent;
 import net.citizensnpcs.api.event.NPCRightClickEvent;
@@ -46,7 +48,21 @@ public class NPCInteractListener implements Listener {
 				|| (event instanceof NPCLeftClickEvent && clickType != ClickType.LEFT)) && clickType != ClickType.ALL) {
 			return;
 		}
-
+		
+		DialoguePermission permissions = dialogue.getPermissions();
+		
+		if(permissions != null && permissions.getPermission() != null) {
+			String permission = permissions.getPermission();
+			String message = permissions.getMessage();
+			
+			if(!player.hasPermission(permission)) {
+				if(message != null) {
+					player.sendMessage(Placeholders.translate(player, message).replace("%npc_name%", dialogue.getDisplayName()));
+				}
+				return;
+			}
+		}
+		
 		if (dialogue.isFirstInteractionEnabled() && !api.wasReadedBy(player, dialogue)) {
 			dialogue.startFirstInteraction(player, true);
 			return;
