fa617bcc6cf5f7eb890b3fd758b7ab5112e38122
iAtog <atogmc58vani@gmail.com>
2022-02-05 19:31:51 -0400

    fix(method): fixed 'index out of bounds exception' in teleport method

--- src/me/iatog/characterdialogue/dialogs/method/TeleportMethod.java
+++ src/me/iatog/characterdialogue/dialogs/method/TeleportMethod.java
@@ -20,14 +20,20 @@ public class TeleportMethod extends DialogMethod<CharacterDialoguePlugin> {
 		// TELEPORT: world,x,y,z,yaw,pitch
 		String[] part = arg.split(",");
 		Location location = player.getLocation();
-		
-		World world = part[0].equals("current") ? player.getWorld() : Bukkit.getWorld(part[0]);
-		double x = Double.valueOf(part[1]);
-		double y = Double.valueOf(part[2]);
-		double z = Double.valueOf(part[3]);
-		float yaw = part[4].equals("current") ? location.getYaw() : Float.valueOf(part[4]);
-		float pitch = part[5].equals("current") ? location.getPitch() : Float.valueOf(part[5]);
-		
+		String currentText = "current";
+
+		World world = part[0].equals(currentText) ? player.getWorld() : Bukkit.getWorld(part[0]);
+		double x = part[1].equals(currentText) ? location.getX() : Double.valueOf(part[1]);
+		double y = part[2].equals(currentText) ? location.getY() : Double.valueOf(part[2]);
+		double z = part[3].equals(currentText) ? location.getZ() : Double.valueOf(part[3]);
+		float yaw = location.getYaw();
+		float pitch = location.getPitch();
+
+		if (part.length > 4) {
+			yaw = Float.valueOf(part[4]);
+			pitch = Float.valueOf(part[5]);
+		}
+
 		Location toTeleport = new Location(world, x, y, z, yaw, pitch);
 		player.teleport(toTeleport);
 	}
