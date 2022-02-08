423a16f40d96511153bccff975a892e217d066b6
iAtog <atogmc58vani@gmail.com>
2022-02-08 09:10:54 -0400

    feat: changed filter to work with log4j
    The previous one was from an incorrect package, it has been changed to
    adapt it to log4j so that it works correctly.

--- src/me/iatog/characterdialogue/filter/ConsoleFilter.java
+++ src/me/iatog/characterdialogue/filter/ConsoleFilter.java
@@ -1,25 +1,121 @@
 package me.iatog.characterdialogue.filter;
 
-import java.util.logging.Filter;
-import java.util.logging.LogRecord;
-import java.util.logging.Logger;
-
-import org.bukkit.Bukkit;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.Marker;
+import org.apache.logging.log4j.core.Filter;
+import org.apache.logging.log4j.core.LifeCycle;
+import org.apache.logging.log4j.core.LogEvent;
+import org.apache.logging.log4j.core.Logger;
+import org.apache.logging.log4j.message.Message;
 
 import me.iatog.characterdialogue.dialogs.method.ChoiceMethod;
 
 public class ConsoleFilter implements Filter {
 
-	@Override
-	public boolean isLoggable(LogRecord record) {
-		return !record.getMessage().contains(ChoiceMethod.COMMAND_NAME);
+	public Filter.Result checkMessage(String message) {
+		if (message.contains(ChoiceMethod.COMMAND_NAME)) {
+			return Filter.Result.DENY;
+		} else {
+			return Filter.Result.NEUTRAL;
+		}
 	}
-	
-	public static void filter(Logger logger) {
-		ConsoleFilter filter = new ConsoleFilter();
-		logger.setFilter(filter);
-		Bukkit.getLogger().setFilter(filter);
-		Logger.getLogger("Minecraft").setFilter(filter);
+
+	public LifeCycle.State getState() {
+		try {
+			return LifeCycle.State.STARTED;
+		} catch (Exception localException) {
+		}
+		return null;
+	}
+
+	public void initialize() {
+	}
+
+	public boolean isStarted() {
+		return true;
+	}
+
+	public boolean isStopped() {
+		return false;
+	}
+
+	public void start() {
+	}
+
+	public void stop() {
+	}
+
+	public Filter.Result filter(LogEvent event) {
+		return checkMessage(event.getMessage().getFormattedMessage());
+	}
+
+	public Filter.Result filter(Logger arg0, Level arg1, Marker arg2, String message, Object... arg4) {
+		return checkMessage(message);
+	}
+
+	public Filter.Result filter(Logger arg0, Level arg1, Marker arg2, String message, Object arg4) {
+		return checkMessage(message);
+	}
+
+	public Filter.Result filter(Logger arg0, Level arg1, Marker arg2, Object message, Throwable arg4) {
+		return checkMessage(message.toString());
+	}
+
+	public Filter.Result filter(Logger arg0, Level arg1, Marker arg2, Message message, Throwable arg4) {
+		return checkMessage(message.getFormattedMessage());
+	}
+
+	public Filter.Result filter(Logger arg0, Level arg1, Marker arg2, String message, Object arg4, Object arg5) {
+		return checkMessage(message);
+	}
+
+	public Filter.Result filter(Logger arg0, Level arg1, Marker arg2, String message, Object arg4, Object arg5,
+			Object arg6) {
+		return checkMessage(message);
+	}
+
+	public Filter.Result filter(Logger arg0, Level arg1, Marker arg2, String message, Object arg4, Object arg5,
+			Object arg6, Object arg7) {
+		return checkMessage(message);
+	}
+
+	public Filter.Result filter(Logger arg0, Level arg1, Marker arg2, String message, Object arg4, Object arg5,
+			Object arg6, Object arg7, Object arg8) {
+		return checkMessage(message);
+	}
+
+	public Filter.Result filter(Logger arg0, Level arg1, Marker arg2, String message, Object arg4, Object arg5,
+			Object arg6, Object arg7, Object arg8, Object arg9) {
+		return checkMessage(message);
+	}
+
+	public Filter.Result filter(Logger arg0, Level arg1, Marker arg2, String message, Object arg4, Object arg5,
+			Object arg6, Object arg7, Object arg8, Object arg9, Object arg10) {
+		return checkMessage(message);
+	}
+
+	public Filter.Result filter(Logger arg0, Level arg1, Marker arg2, String message, Object arg4, Object arg5,
+			Object arg6, Object arg7, Object arg8, Object arg9, Object arg10, Object arg11) {
+		return checkMessage(message);
+	}
+
+	public Filter.Result filter(Logger arg0, Level arg1, Marker arg2, String message, Object arg4, Object arg5,
+			Object arg6, Object arg7, Object arg8, Object arg9, Object arg10, Object arg11, Object arg12) {
+		return checkMessage(message);
+	}
+
+	public Filter.Result filter(Logger arg0, Level arg1, Marker arg2, String message, Object arg4, Object arg5,
+			Object arg6, Object arg7, Object arg8, Object arg9, Object arg10, Object arg11, Object arg12,
+			Object arg13) {
+		return checkMessage(message);
+	}
+
+	public Filter.Result getOnMatch() {
+		return Filter.Result.NEUTRAL;
+	}
+
+	public Filter.Result getOnMismatch() {
+		return Filter.Result.NEUTRAL;
 	}
 
 }
--- src/me/iatog/characterdialogue/loader/PluginLoader.java
+++ src/me/iatog/characterdialogue/loader/PluginLoader.java
@@ -3,6 +3,9 @@ package me.iatog.characterdialogue.loader;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.Logger;
+
 import me.iatog.characterdialogue.CharacterDialoguePlugin;
 import me.iatog.characterdialogue.filter.ConsoleFilter;
 
@@ -18,8 +21,8 @@ public class PluginLoader implements Loader {
 	
 	@Override
 	public void load() {
-		ConsoleFilter.filter(main.getLogger());
-		
+		((Logger) LogManager.getRootLogger()).addFilter(new ConsoleFilter());
+
 		loadLoaders(
 				new ListenerLoader(main),
 				new FileLoader(main),
