869f0f86d07539d19fdd2cd4220fc3952b58d30c
iAtog <atogmc58vani@gmail.com>
2022-04-15 16:14:04 -0400

    style: changed api style

--- api/src/main/java/com/github/iatog/characterdialogue/api/CharacterDialogueAPI.java
+++ api/src/main/java/com/github/iatog/characterdialogue/api/CharacterDialogueAPI.java
@@ -1,5 +1,23 @@
 package com.github.iatog.characterdialogue.api;
 
-public interface CharacterDialogueAPI {
+import java.util.Map;
 
+import com.github.iatog.characterdialogue.api.dialogue.Dialogue;
+
+public interface CharacterDialogueAPI {
+    String getDialogueByNPC(int id);
+
+    Dialogue getDialogue(String name);
+
+    void reloadHolograms();
+
+    void loadHologram(int npcId);
+
+    Dialogue getNPCDialogue(int id);
+
+    Map<String, Dialogue> getDialogues();
+
+    String getNPCDialogueName(int id);
+
+    int getBukkitVersion();
 }
