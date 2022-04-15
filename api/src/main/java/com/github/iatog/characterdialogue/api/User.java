9e8d7ba02b3368ac073b2a2e0b9db5dca14aba2f
iAtog <atogmc58vani@gmail.com>
2022-04-15 16:12:47 -0400

    feat: created user object class for player managing

---
+++ api/src/main/java/com/github/iatog/characterdialogue/api/User.java
@@ -0,0 +1,33 @@
+package com.github.iatog.characterdialogue.api;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+import org.bukkit.entity.Player;
+
+import com.github.iatog.characterdialogue.api.dialogue.Dialogue;
+import com.github.iatog.characterdialogue.api.dialogue.DialogueLine;
+import com.github.iatog.characterdialogue.api.dialogue.DialogueSession;
+
+public interface User {
+    
+    boolean readDialogue(String dialogue);
+    boolean readDialogue(Dialogue dialogue);
+    boolean readedDialogue(Dialogue dialogue);
+    boolean readedDialogue(String dialogue);
+
+    void runDialogue(Dialogue dialogue);
+    void runDialogue(String dialogueName);
+
+    void runDialogueExpression(String dialog);
+    void runDialogueExpression(String dialog, String npcName);
+    void runDialogueExpression(String dialog, String npcName, Consumer<DialogueLine> fail, DialogueSession session);
+    void runDialogueExpressions(List<String> lines, ClickType type, int npcId, String displayName);
+    void runDialogueExpressions(List<String> lines, String displayName);
+
+    boolean setMovement(boolean movement);
+    boolean canEnableMovement();
+    void sendMessage(String message);
+    
+    Player toPlayer();
+}
