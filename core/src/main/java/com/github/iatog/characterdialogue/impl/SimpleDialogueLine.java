ce335f25d4dc54ce52ff82072dc04e5e3d73df36
iAtog <atogmc58vani@gmail.com>
2022-04-16 10:41:55 -0400

    chore: added npcName configuration

--- core/src/main/java/com/github/iatog/characterdialogue/impl/SimpleDialogueLine.java
+++ core/src/main/java/com/github/iatog/characterdialogue/impl/SimpleDialogueLine.java
@@ -4,20 +4,27 @@ import org.bukkit.entity.Player;
 
 import com.github.iatog.characterdialogue.api.dialogue.DialogueLine;
 import com.github.iatog.characterdialogue.api.dialogue.DialogueSession;
-import com.github.iatog.characterdialogue.api.method.Method;
+import com.github.iatog.characterdialogue.api.method.AbstractMethod;
+import com.github.iatog.characterdialogue.util.StringUtil;
 
 public class SimpleDialogueLine implements DialogueLine {
 
-    private final Method method;
+    private final AbstractMethod method;
     private final String argument;
+    private String npcName = getNPCName();
     
-    public SimpleDialogueLine(Method method, String argument) {
+    public SimpleDialogueLine(AbstractMethod method, String argument) {
         this.method = method;
         this.argument = argument;
     }
     
+    public SimpleDialogueLine(AbstractMethod method, String argument, String npcName) {
+        this(method, argument);
+        this.npcName = npcName;
+    }
+    
     @Override
-    public Method getMethod() {
+    public AbstractMethod getMethod() {
         return method;
     }
 
@@ -25,15 +32,21 @@ public class SimpleDialogueLine implements DialogueLine {
     public String getArgument() {
         return argument;
     }
+    
+    @Override
+    public String getNPCName() {
+        return npcName;
+    }
 
     @Override
     public void run(Player player, DialogueSession session) {
-        
+        String translated = StringUtil.translatePlaceholders(player, argument.replace("%npc_name%", getNPCName()));
+        method.run(player, translated, session);
     }
 
     @Override
     public void runSingle(Player player) {
-        
+        run(player, null);
     }
 
 }
