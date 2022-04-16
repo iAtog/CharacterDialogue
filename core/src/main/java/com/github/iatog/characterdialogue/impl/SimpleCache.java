b2adc480c939e12737c54ea2015e81eb00965cfe
iAtog <atogmc58vani@gmail.com>
2022-04-16 11:03:07 -0400

    feat(core): created universal cache

---
+++ core/src/main/java/com/github/iatog/characterdialogue/impl/SimpleCache.java
@@ -0,0 +1,42 @@
+package com.github.iatog.characterdialogue.impl;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.BiConsumer;
+
+import com.github.iatog.characterdialogue.api.cache.Cache;
+
+public class SimpleCache<K, V> implements Cache<K, V> {
+    
+    private final Map<K, V> map;
+    
+    public SimpleCache() {
+        this.map = new ConcurrentHashMap<>();
+    }
+    
+    @Override
+    public V get(K key) {
+        return map.get(key);
+    }
+
+    @Override
+    public void set(K key, V value) {
+        map.put(key, value);
+    }
+
+    @Override
+    public boolean contains(K key) {
+        return map.containsKey(key);
+    }
+
+    @Override
+    public V remove(K key) {
+        return map.remove(key);
+    }
+
+    @Override
+    public void forEach(BiConsumer<K, V> consumer) {
+        map.forEach(consumer);
+    }
+
+}
