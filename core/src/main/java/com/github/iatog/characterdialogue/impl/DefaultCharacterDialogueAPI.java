c73e6ce32e6c52ca7455378df369fc5c0efe7c72
iAtog <atogmc58vani@gmail.com>
2022-04-15 22:56:25 -0400

    feat(api): finised cache implementation

--- core/src/main/java/com/github/iatog/characterdialogue/impl/DefaultCharacterDialogueAPI.java
+++ core/src/main/java/com/github/iatog/characterdialogue/impl/DefaultCharacterDialogueAPI.java
@@ -11,43 +11,36 @@ public class DefaultCharacterDialogueAPI implements CharacterDialogueAPI {
 
     @Override
     public String getDialogueByNPC(int id) {
-        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public Dialogue getDialogue(String name) {
-        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public void reloadHolograms() {
-        // TODO Auto-generated method stub
-        
+
     }
 
     @Override
     public void loadHologram(int npcId) {
-        // TODO Auto-generated method stub
         
     }
 
     @Override
     public Dialogue getNPCDialogue(int id) {
-        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public Map<String, Dialogue> getDialogues() {
-        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public String getNPCDialogueName(int id) {
-        // TODO Auto-generated method stub
         return null;
     }
 
@@ -55,6 +48,4 @@ public class DefaultCharacterDialogueAPI implements CharacterDialogueAPI {
     public int getBukkitVersion() {
         return Integer.parseInt(Bukkit.getBukkitVersion().split("-")[0].split("\\.")[1]);
     }
-
-
 }
--- core/src/main/java/com/github/iatog/characterdialogue/impl/SimpleUserCache.java
+++ core/src/main/java/com/github/iatog/characterdialogue/impl/SimpleUserCache.java
@@ -1,5 +1,38 @@
 package com.github.iatog.characterdialogue.impl;
 
-public class SimpleUserCache {
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+import com.github.iatog.characterdialogue.api.Cache;
+import com.github.iatog.characterdialogue.api.User;
+
+public class SimpleUserCache implements Cache<UUID, User> {
+    
+    private Map<UUID, User> users;
+    
+    public SimpleUserCache() {
+        this.users = new ConcurrentHashMap<>();
+    }
+    
+    @Override
+    public User get(UUID key) {
+        return users.get(key);
+    }
+
+    @Override
+    public void set(UUID key, User value) {
+        users.put(key, value);
+    }
+
+    @Override
+    public boolean contains(UUID key) {
+        return users.containsKey(key);
+    }
+
+    @Override
+    public User remove(UUID key) {
+        return users.remove(key);
+    }
 
 }
