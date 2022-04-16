f49fe712d9ee534c9302facb8e08fe7a2ef61cab
iAtog <atogmc58vani@gmail.com>
2022-04-16 10:40:44 -0400

    feat(api): created npc name def getter

--- api/src/main/java/com/github/iatog/characterdialogue/api/dialogue/DialogueLine.java
+++ api/src/main/java/com/github/iatog/characterdialogue/api/dialogue/DialogueLine.java
@@ -2,12 +2,15 @@ package com.github.iatog.characterdialogue.api.dialogue;
 
 import org.bukkit.entity.Player;
 
-import com.github.iatog.characterdialogue.api.method.Method;
+import com.github.iatog.characterdialogue.api.method.AbstractMethod;
 
 public interface DialogueLine {
     
-    Method getMethod();
+    AbstractMethod getMethod();
     String getArgument();
+    default String getNPCName() {
+        return "Jonh Doe";
+    }
     
     void run(Player player, DialogueSession session);
     void runSingle(Player player);
