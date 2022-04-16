8a833fb8b409e5f42fa0bbeb6063e5c89e8c7284
iAtog <atogmc58vani@gmail.com>
2022-04-16 10:40:08 -0400

    feat(api): added dialogue session getter

--- api/src/main/java/com/github/iatog/characterdialogue/api/method/Method.java
+++ api/src/main/java/com/github/iatog/characterdialogue/api/method/Method.java
@@ -2,9 +2,12 @@ package com.github.iatog.characterdialogue.api.method;
 
 import org.bukkit.entity.Player;
 
+import com.github.iatog.characterdialogue.api.dialogue.DialogueSession;
+
 public interface Method {
     
     public Player getPlayer();
     public String getArgument();
+    public DialogueSession getSession();
     
 }
