92c2a8650ef225d690d1046f60395ecd37b4685a
iAtog <atogmc58vani@gmail.com>
2022-04-16 11:03:55 -0400

    feat(core): added methods to bindings

--- core/src/main/java/com/github/iatog/characterdialogue/module/CacheModule.java
+++ core/src/main/java/com/github/iatog/characterdialogue/module/CacheModule.java
@@ -3,8 +3,9 @@ package com.github.iatog.characterdialogue.module;
 import java.util.UUID;
 
 import com.github.iatog.characterdialogue.api.cache.Cache;
+import com.github.iatog.characterdialogue.api.method.AbstractMethod;
 import com.github.iatog.characterdialogue.api.user.User;
-import com.github.iatog.characterdialogue.impl.SimpleUserCache;
+import com.github.iatog.characterdialogue.impl.SimpleCache;
 
 import team.unnamed.inject.Binder;
 import team.unnamed.inject.Module;
@@ -12,9 +13,11 @@ import team.unnamed.inject.Module;
 public class CacheModule implements Module {
     
     private Cache<UUID, User> users;
+    private Cache<String, AbstractMethod> methods;
     
     public CacheModule() {
-        this.users = new SimpleUserCache();
+        this.users = new SimpleCache<UUID, User>();
+        this.methods = new SimpleCache<String, AbstractMethod>();
     }
     
     @Override
@@ -23,6 +26,10 @@ public class CacheModule implements Module {
         binder.bind(Cache.class)
                 .named("users")
                 .toInstance(users);
+        
+        binder.bind(Cache.class)
+                .named("methods")
+                .toInstance(methods);
         
     }
 
