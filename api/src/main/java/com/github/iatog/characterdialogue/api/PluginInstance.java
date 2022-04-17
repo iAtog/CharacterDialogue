41a521af14a82e1af8c78ee9f0cefb24afbec596
iAtog <atogmc58vani@gmail.com>
2022-04-17 03:27:46 -0400

    feat(core): added cache factory to plugin

--- api/src/main/java/com/github/iatog/characterdialogue/api/PluginInstance.java
+++ api/src/main/java/com/github/iatog/characterdialogue/api/PluginInstance.java
@@ -1,17 +1,11 @@
 package com.github.iatog.characterdialogue.api;
 
-import java.util.UUID;
-
-import com.github.iatog.characterdialogue.api.cache.Cache;
-import com.github.iatog.characterdialogue.api.method.AbstractMethod;
-import com.github.iatog.characterdialogue.api.user.User;
+import com.github.iatog.characterdialogue.api.cache.CacheFactory;
 
 public interface PluginInstance {
 
     CharacterDialogueAPI getAPI();
     
-    Cache<UUID, User> getUsers();
-    
-    Cache<String, AbstractMethod> getMethods();
+    CacheFactory getCacheFactory();
     
 }
