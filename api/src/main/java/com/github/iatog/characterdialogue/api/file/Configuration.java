2b9a9b4342a21a8fd120eb7d039dad4084ce009f
iAtog <atogmc58vani@gmail.com>
2022-04-19 15:14:39 -0400

    refactor: moved Configuration to api

--- api/src/main/java/com/github/iatog/characterdialogue/api/file/Configuration.java
+++ api/src/main/java/com/github/iatog/characterdialogue/api/file/Configuration.java
@@ -1,4 +1,4 @@
-package com.github.iatog.characterdialogue.util;
+package com.github.iatog.characterdialogue.api.file;
 
 import org.bukkit.ChatColor;
 import org.bukkit.configuration.InvalidConfigurationException;
