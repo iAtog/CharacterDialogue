40e40e8754af5d0fa870023a36754bb63a062665
iAtog <atogmc58vani@gmail.com>
2022-04-20 23:46:09 -0400

    chore: added getters to PluginInstance

--- api/src/main/java/com/github/iatog/characterdialogue/api/PluginInstance.java
+++ api/src/main/java/com/github/iatog/characterdialogue/api/PluginInstance.java
@@ -1,11 +1,21 @@
 package com.github.iatog.characterdialogue.api;
 
+import java.util.logging.Logger;
+
 import com.github.iatog.characterdialogue.api.cache.CacheFactory;
+import com.github.iatog.characterdialogue.api.file.DialogueFileManager;
+import com.github.iatog.characterdialogue.api.file.YamlFileRegistry;
 
 public interface PluginInstance {
 
     CharacterDialogueAPI getAPI();
     
     CacheFactory getCacheFactory();
+    
+    Logger getLogger();
+    
+    YamlFileRegistry getFileRegistry();
+    
+    DialogueFileManager getDialogueManager();
     
 }
