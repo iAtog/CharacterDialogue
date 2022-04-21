5072094b10e330fb7d2a4fed79d2cb018e797ac1
iAtog <atogmc58vani@gmail.com>
2022-04-20 23:48:06 -0400

    feat(cache): created dialogues cache

--- core/src/main/java/com/github/iatog/characterdialogue/module/CacheModule.java
+++ core/src/main/java/com/github/iatog/characterdialogue/module/CacheModule.java
@@ -4,11 +4,12 @@ import java.util.UUID;
 
 import com.github.iatog.characterdialogue.api.cache.Cache;
 import com.github.iatog.characterdialogue.api.cache.CacheFactory;
+import com.github.iatog.characterdialogue.api.dialogue.Dialogue;
 import com.github.iatog.characterdialogue.api.dialogue.DialogueSession;
 import com.github.iatog.characterdialogue.api.impl.CacheFactoryImpl;
+import com.github.iatog.characterdialogue.api.impl.SimpleCache;
 import com.github.iatog.characterdialogue.api.method.AbstractMethod;
 import com.github.iatog.characterdialogue.api.user.User;
-import com.github.iatog.characterdialogue.impl.SimpleCache;
 
 import team.unnamed.inject.Binder;
 import team.unnamed.inject.Module;
@@ -20,13 +21,16 @@ public class CacheModule implements Module {
     private Cache<UUID, DialogueSession> sessions;
     private Cache<UUID, User> users;
     private Cache<String, AbstractMethod> methods;
+    private Cache<String, Dialogue> dialogues;
     
     public CacheModule() {
         this.sessions = new SimpleCache<>();
         this.users = new SimpleCache<>();
         this.methods = new SimpleCache<>();
+        this.dialogues = new SimpleCache<>();
         
-        this.factory = new CacheFactoryImpl(sessions, methods, users);
+        
+        this.factory = new CacheFactoryImpl(sessions, methods, users, dialogues);
     }
     
     @Override
