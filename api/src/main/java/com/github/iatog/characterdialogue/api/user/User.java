3f84eb4dfd3604548a4da5696667ec9f43db30d3
iAtog <atogmc58vani@gmail.com>
2022-05-03 22:11:24 -0400

    feat: finished some user methods

--- api/src/main/java/com/github/iatog/characterdialogue/api/user/User.java
+++ api/src/main/java/com/github/iatog/characterdialogue/api/user/User.java
@@ -5,6 +5,7 @@ import java.util.function.Consumer;
 
 import org.bukkit.entity.Player;
 
+import com.github.iatog.characterdialogue.api.CharacterDialogueAPI;
 import com.github.iatog.characterdialogue.api.dialogue.Dialogue;
 import com.github.iatog.characterdialogue.api.dialogue.DialogueLine;
 import com.github.iatog.characterdialogue.api.dialogue.DialogueSession;
@@ -14,23 +15,11 @@ public interface User {
 
     boolean readDialogue(String dialogue);
 
-    boolean readDialogue(Dialogue dialogue);
-
-    boolean readedDialogue(Dialogue dialogue);
-
     boolean readedDialogue(String dialogue);
 
     void runDialogue(Dialogue dialogue);
 
-    void runDialogue(String dialogueName);
-
-    void runDialogueExpression(String method, String argument);
-
-    void runDialogueExpression(String method, String argument, String npcName);
-
     void runDialogueExpression(String method, String argument, String npcName, Consumer<DialogueLine> fail, DialogueSession session);
-
-    void runDialogueExpressions(List<DialogueLine> lines, ClickType type, int npcId, String displayName);
 
     void runDialogueExpressions(List<DialogueLine> lines, String displayName);
 
@@ -41,4 +30,25 @@ public interface User {
     void sendMessage(String message);
 
     Player toPlayer();
+
+    default void runDialogueExpression(String method, String argument) {
+        runDialogueExpression(method, argument, "John");
+    }
+
+    default void runDialogueExpression(String method, String argument, String npcName) {
+        runDialogueExpression(method, argument, npcName, (l) -> {
+        }, null);
+    }
+
+    default void runDialogue(String dialogueName) {
+        runDialogue(CharacterDialogueAPI.create().getPlugin().getCacheFactory().getDialogues().get(dialogueName));
+    }
+
+    default boolean readDialogue(Dialogue dialogue) {
+        return readDialogue(dialogue.getName());
+    }
+
+    default boolean readedDialogue(Dialogue dialogue) {
+        return readedDialogue(dialogue.getName());
+    }
 }
--- core/src/main/java/com/github/iatog/characterdialogue/impl/UserImpl.java
+++ core/src/main/java/com/github/iatog/characterdialogue/impl/UserImpl.java
@@ -1,48 +1,69 @@
 package com.github.iatog.characterdialogue.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 import java.util.function.Consumer;
 
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
+import com.github.iatog.characterdialogue.CharacterDialoguePlugin;
+import com.github.iatog.characterdialogue.api.cache.Cache;
 import com.github.iatog.characterdialogue.api.dialogue.Dialogue;
 import com.github.iatog.characterdialogue.api.dialogue.DialogueLine;
 import com.github.iatog.characterdialogue.api.dialogue.DialogueSession;
+import com.github.iatog.characterdialogue.api.file.YamlFile;
+import com.github.iatog.characterdialogue.api.impl.DefaultDialogueSession;
+import com.github.iatog.characterdialogue.api.method.AbstractMethod;
 import com.github.iatog.characterdialogue.api.types.ClickType;
 import com.github.iatog.characterdialogue.api.user.User;
+import com.github.iatog.characterdialogue.util.StringUtil;
 
 public class UserImpl implements User {
     
-    private UUID uuid;
+    private final CharacterDialoguePlugin PLUGIN = CharacterDialoguePlugin.getInstance();
+    
+    private Player player;
     
     public UserImpl(UUID uuid) {
-        this.uuid = uuid;
+        this.player = Bukkit.getPlayer(uuid);
     }
     
     @Override
     public boolean readDialogue(String dialogue) {
-        // TODO Auto-generated method stub
-        return false;
-    }
+        YamlFile playerCache = PLUGIN.getFileRegistry().getFile("player-cache");
+        String path = "players." + player.getUniqueId();
+        List<String> readedDialogues = playerCache.getStringList(path + ".readed-dialogues");
 
-    @Override
-    public boolean readDialogue(Dialogue dialogue) {
-        // TODO Auto-generated method stub
-        return false;
+        if (readedDialogues == null) {
+            readedDialogues = new ArrayList<>();
+        }
+
+        if (readedDialogues.contains(dialogue)) {
+            return false;
+        }
+
+        readedDialogues.add(dialogue);
+        playerCache.set(path + ".readed-dialogues", readedDialogues);
+        playerCache.save();
+        return true;
     }
 
     @Override
     public boolean readedDialogue(Dialogue dialogue) {
-        // TODO Auto-generated method stub
-        return false;
+        return readedDialogue(dialogue.getName());
     }
 
     @Override
     public boolean readedDialogue(String dialogue) {
-        // TODO Auto-generated method stub
-        return false;
+        YamlFile playerCache = PLUGIN.getFileRegistry().getFile("player-cache");
+        String path = "players." + player.getUniqueId();
+        List<String> readedDialogues = playerCache.getStringList(path + ".readed-dialogues");
+
+        return playerCache.contains(path) && readedDialogues.contains(dialogue);
     }
 
     @Override
@@ -51,53 +72,67 @@ public class UserImpl implements User {
     }
 
     @Override
-    public void runDialogue(String dialogueName) {
-        
-    }
-
-    @Override
-    public void runDialogueExpression(String method, String argument) {
-        
-    }
-
-    @Override
-    public void runDialogueExpression(String method, String argument, String npcName) {
-        
-    }
-
-    @Override
     public void runDialogueExpression(String method, String argument, String npcName, Consumer<DialogueLine> fail, DialogueSession session) {
+        Cache<String, AbstractMethod> methods = PLUGIN.getCacheFactory().getMethods();
         
-    }
-
-    @Override
-    public void runDialogueExpressions(List<DialogueLine> lines, ClickType type, int npcId, String displayName) {
+        if(!methods.contains(method)) {
+            PLUGIN.getLogger().warning(String.format("Invalid method in dialog line (%s)", method));
+            return;
+        }
+        
         
     }
 
     @Override
     public void runDialogueExpressions(List<DialogueLine> lines, String displayName) {
-        
+        Cache<UUID, DialogueSession> sessions = PLUGIN.getCacheFactory().getDialogueSessions();
+        if (sessions.contains(player.getUniqueId())) {
+            return;
+        }
+
+        DialogueSession session = new DefaultDialogueSession(player, displayName == null ? "John" : displayName, lines);
+
+        sessions.set(player.getUniqueId(), session);
+        session.start();
     }
 
     @Override
     public boolean setMovement(boolean movement) {
-        return false;
+        YamlFile playerCache = PLUGIN.getFileRegistry().getFile("player-cache");
+        String path = "players." + player.getUniqueId();
+
+        if(movement) {
+            float speed = playerCache.getFloat(path + ".last-speed");
+
+            player.setWalkSpeed(speed);
+            player.removePotionEffect(PotionEffectType.JUMP);
+        } else {
+            playerCache.set(path + ".last-speed", player.getWalkSpeed());
+            player.setWalkSpeed(0);
+            player.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, Integer.MAX_VALUE, 128));
+        }
+
+        playerCache.set(path + ".remove-effect", !movement);
+        playerCache.save();
+        return !movement;
     }
 
     @Override
     public boolean canEnableMovement() {
-        return false;
+        YamlFile playerCache = PLUGIN.getFileRegistry().getFile("player-cache");
+        String path = "players." + player.getUniqueId();
+
+        return playerCache.getBoolean(path + ".remove-effect", false);
     }
 
     @Override
     public void sendMessage(String message) {
-        
+        player.sendMessage(StringUtil.colorize(message));
     }
 
     @Override
     public Player toPlayer() {
-        return Bukkit.getPlayer(uuid);
+        return player;
     }
 
 }
