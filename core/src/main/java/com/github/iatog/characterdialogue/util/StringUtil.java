e5aefecd5a3111beaeb820dbb32d336ca048eeed
iAtog <atogmc58vani@gmail.com>
2022-05-14 17:27:29 -0400

    feat: created placeholders translation

--- core/src/main/java/com/github/iatog/characterdialogue/util/StringUtil.java
+++ core/src/main/java/com/github/iatog/characterdialogue/util/StringUtil.java
@@ -6,6 +6,9 @@ import java.util.regex.Pattern;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
+import com.github.iatog.characterdialogue.CharacterDialoguePlugin;
+import com.github.iatog.characterdialogue.api.file.YamlFile;
+
 import net.md_5.bungee.api.ChatColor;
 
 import static org.bukkit.ChatColor.translateAlternateColorCodes;
@@ -14,6 +17,7 @@ public class StringUtil {
     
     private static final boolean SUPPORTS_HEX = Integer.parseInt(Bukkit.getBukkitVersion().split("-")[0].split("\\.")[1]) >= 16;
     private static final Pattern HEX_PATTERN = Pattern.compile("&#[a-fA-F0-9]{6}");
+    private static final CharacterDialoguePlugin PLUGIN = CharacterDialoguePlugin.getInstance();
     
     public static String colorize(String message) {
         if (SUPPORTS_HEX) {
@@ -30,7 +34,19 @@ public class StringUtil {
     }
 
     public static String translatePlaceholders(Player player, String string) {
-        return string;
+        YamlFile config = PLUGIN.getFileRegistry().getFile("config");
+        
+        for(String name : config.getConfigurationSection("placeholders").getKeys(false)) {
+            String value = config.getString("placeholders."+name);
+            string = string.replace("%" + name + "%", value);
+        }
+        
+        
+        return colorize(string);
+    }
+    
+    public static String translatePlaceholders(Player player, String string, String npcName) {
+        return translatePlaceholders(player, string.replace("%npc_name%", npcName));
     }
     
 }
