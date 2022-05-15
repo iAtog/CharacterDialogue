8c03349a31e8d2a7808c9d3bf22ed2d6bc87c503
iAtog <atogmc58vani@gmail.com>
2022-05-15 17:37:22 -0400

    feat(core): added dialogue execution on interact

--- core/src/main/java/com/github/iatog/characterdialogue/listener/NPCListener.java
+++ core/src/main/java/com/github/iatog/characterdialogue/listener/NPCListener.java
@@ -1,12 +1,19 @@
 package com.github.iatog.characterdialogue.listener;
 
+import java.util.UUID;
+
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
 import com.github.iatog.characterdialogue.api.CharacterDialogueAPI;
+import com.github.iatog.characterdialogue.api.cache.Cache;
 import com.github.iatog.characterdialogue.api.dialogue.Dialogue;
+import com.github.iatog.characterdialogue.api.dialogue.Dialogue.DialoguePermission;
+import com.github.iatog.characterdialogue.api.dialogue.DialogueSession;
 import com.github.iatog.characterdialogue.api.types.ClickType;
+import com.github.iatog.characterdialogue.api.user.User;
+import com.github.iatog.characterdialogue.util.StringUtil;
 
 import net.citizensnpcs.api.event.NPCClickEvent;
 import net.citizensnpcs.api.event.NPCLeftClickEvent;
@@ -40,10 +47,32 @@ public class NPCListener implements Listener {
         Player player = event.getClicker();
         int npcId = event.getNPC().getId();
         Dialogue dialogue = API.getNPCDialogue(npcId);
+        Cache<UUID, DialogueSession> sessions = API.getPlugin().getCacheFactory().getDialogueSessions();
+        User user = API.getUser(player);
+
+        if(dialogue == null || sessions.contains(player.getUniqueId())) {
+            return;
+        }
+
+        DialoguePermission permissions = dialogue.getPermissions();
+
+        if(permissions != null && permissions.getPermission() != null) {
+            String permission = permissions.getPermission();
+            String message = permissions.getMessage();
+            
+            if(!player.hasPermission(permission)) {
+                if(message != null) {
+                    player.sendMessage(StringUtil.translatePlaceholders(player, message, dialogue.getDisplayName()));
+                }
+                return;
+            }
+        }
+
+        if (dialogue.isFirstInteractionEnabled() && user.readedDialogue(dialogue)) {
+            dialogue.startFirstInteraction(player, true);
+            return;
+        }
         
-        
+        dialogue.start(player);
     }
-    
-    
-    
 }
