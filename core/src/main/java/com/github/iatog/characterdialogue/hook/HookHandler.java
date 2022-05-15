88111d68da1bff7b3fd357e9eaf4b7c8cb5fb94b
iAtog <atogmc58vani@gmail.com>
2022-05-15 13:15:50 -0400

    chore: ¿?

--- core/src/main/java/com/github/iatog/characterdialogue/hook/HookHandler.java
+++ core/src/main/java/com/github/iatog/characterdialogue/hook/HookHandler.java
@@ -9,34 +9,34 @@ import com.github.iatog.characterdialogue.hook.hologram.HolographicDisplaysHook;
 import com.github.iatog.characterdialogue.hook.hologram.InvalidHologramsHook;
 
 public class HookHandler {
-    
+
     private CharacterDialoguePlugin main;
-    
+
     private HologramHook hologram;
-    
+
     public HookHandler(CharacterDialoguePlugin main) {
         this.main = main;
 
         setup();
     }
-    
+
     private void setup() {
         // hologram hook part
-        if(isEnabled("DecentHolograms")) {
+        if (isEnabled("DecentHolograms")) {
             this.hologram = new DecentHologramsHook(main);
-        } else if(isEnabled("PlaceHolderAPI")) {
+        } else if (isEnabled("PlaceHolderAPI")) {
             this.hologram = new HolographicDisplaysHook(main);
         } else {
             this.hologram = new InvalidHologramsHook(main);
         }
     }
-    
+
     public HologramHook getHologramHook() {
         return hologram;
     }
-    
+
     private boolean isEnabled(String plugin) {
         return Bukkit.getPluginManager().isPluginEnabled(plugin);
     }
-    
+
 }
