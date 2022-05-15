f430d6b3823fde4432deee97fe0a55b8fde44d6c
iAtog <atogmc58vani@gmail.com>
2022-05-15 13:16:34 -0400

    feat(core): added dialogue registration

--- core/src/main/java/com/github/iatog/characterdialogue/service/DialogueService.java
+++ core/src/main/java/com/github/iatog/characterdialogue/service/DialogueService.java
@@ -1,8 +1,15 @@
 package com.github.iatog.characterdialogue.service;
 
+import java.util.Map;
+
 import javax.inject.Inject;
 
 import com.github.iatog.characterdialogue.CharacterDialoguePlugin;
+import com.github.iatog.characterdialogue.api.cache.Cache;
+import com.github.iatog.characterdialogue.api.dialogue.Dialogue;
+import com.github.iatog.characterdialogue.api.file.DialogueFileManager;
+import com.github.iatog.characterdialogue.api.file.YamlFile;
+import com.github.iatog.characterdialogue.api.impl.DialogueImpl;
 import com.github.iatog.characterdialogue.api.service.Service;
 
 public class DialogueService implements Service {
@@ -12,7 +19,16 @@ public class DialogueService implements Service {
 
     @Override
     public void start() {
-        // load dialogues
+        Cache<String, Dialogue> dialogues = main.getCacheFactory().getDialogues();
+        DialogueFileManager manager = main.getDialogueManager();
+        
+        for(Map.Entry<String, YamlFile> entry : manager.getDialogues().entrySet()) {
+            String id = entry.getKey();
+            YamlFile file = entry.getValue();
+            
+            dialogues.set(id, new DialogueImpl(main, id, file));
+            main.getLogger().info("Loaded dialogue \"" + id + "\" succesfully");
+        }
     }
 
 }
