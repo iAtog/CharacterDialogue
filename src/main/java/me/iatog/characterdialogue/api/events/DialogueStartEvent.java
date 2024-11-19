92cbf3d789866649f17bf8b34bdfd1b3bb25c13d
iAtog <atogmc58vani@gmail.com>
2024-11-19 12:02:48 -0400

    feat: create dialogue start event

---
+++ src/main/java/me/iatog/characterdialogue/api/events/DialogueStartEvent.java
@@ -0,0 +1,30 @@
+package me.iatog.characterdialogue.api.events;
+
+import me.iatog.characterdialogue.session.DialogSession;
+import org.bukkit.entity.Player;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.player.PlayerEvent;
+import org.jetbrains.annotations.NotNull;
+
+public class DialogueStartEvent extends PlayerEvent {
+    private static final HandlerList handlers = new HandlerList();
+
+    private final DialogSession session;
+
+    public DialogueStartEvent(Player player, DialogSession session) {
+        super(player);
+        this.session = session;
+    }
+
+    public DialogSession getSession() {
+        return session;
+    }
+
+    public static HandlerList getHandlerList(){
+        return handlers;
+    }
+    public final @NotNull HandlerList getHandlers(){
+        return handlers;
+    }
+
+}
