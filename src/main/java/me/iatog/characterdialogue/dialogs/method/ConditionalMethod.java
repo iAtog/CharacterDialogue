ae76f79e7a7dc800e5ca394edc2c27dabcf85cdd
iAtog <atogmc58vani@gmail.com>
2024-11-19 02:06:17 -0400

    chore: ConditionalMethod logic

--- src/main/java/me/iatog/characterdialogue/dialogs/method/ConditionalMethod.java
+++ src/main/java/me/iatog/characterdialogue/dialogs/method/ConditionalMethod.java
@@ -1,5 +1,6 @@
 package me.iatog.characterdialogue.dialogs.method;
 
+import me.iatog.characterdialogue.util.TextUtils;
 import org.bukkit.entity.Player;
 
 import me.iatog.characterdialogue.CharacterDialoguePlugin;
@@ -15,7 +16,7 @@ public class ConditionalMethod extends DialogMethod<CharacterDialoguePlugin> {
 		 * lines:
 		 * - send: hello
 		 * - conditional:        CONDITIONAL    |               CONDITIONAL = TRUE                           |  CONDITIONAL = FALSE
-		 * - conditional: %playername% == aatog | (RUN_DIALOGUE/STOP_AND_SEND_MESSAGE/STOP/RUN_METHOD) %arg% | (METHODS)
+		 * - conditional: %playername% == aatog | (RUN_DIALOGUE/STOP_AND_SEND_MESSAGE/STOP/RUN_METHOD): %arg% | (METHODS)
 		 * - conditional: %time% > 5000 | STOP_AND_SEND_MESSAGE: %npc_name%: It would be better to talk about it later
 		 * - conditional: %kills% == 20 |
 		 */
@@ -24,10 +25,30 @@ public class ConditionalMethod extends DialogMethod<CharacterDialoguePlugin> {
 	@SuppressWarnings("unused")
 	@Override
 	public void execute(Player player, String arg, DialogSession session) {
-		String condition = arg.split(":")[0];
-		String toExecute = arg.substring(condition.length());
-		
-		
+		//String condition = arg.split(":")[0];
+		//String toExecute = arg.substring(condition.length());
+		String[] arguments = arg.split("\\|");
+		String condition = arguments[0].trim();
+		String ifTrue = arguments[1].trim();
+		String ifFalse = arguments[2].trim();
+
+		boolean conditionResult = false;
+
+		try {
+			conditionResult = evaluateCondition(player, condition);
+		} catch(IllegalArgumentException e) {
+			player.sendMessage(TextUtils.colorize("&cFatal error occurred."));
+			getProvider().getLogger().warning("The dialogue '" + session.getDialogue().getName() + "' has an invalid condition in L" + session.getCurrentIndex());
+			session.destroy();
+			return;
+		}
+
+
+		if(conditionResult) {
+			// ifTrue stuff
+		} else {
+			// ifFalse estufa
+		}
 	}
 
 	public boolean evaluateCondition(Player player, String condition) {
