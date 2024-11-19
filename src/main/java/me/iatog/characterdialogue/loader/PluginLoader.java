dba947341beb9e552b1feb5b3c2579140cf276eb
iAtog <atogmc58vani@gmail.com>
2024-11-19 01:51:37 -0400

    refactor: use lambda

--- src/main/java/me/iatog/characterdialogue/loader/PluginLoader.java
+++ src/main/java/me/iatog/characterdialogue/loader/PluginLoader.java
@@ -3,6 +3,7 @@ package me.iatog.characterdialogue.loader;
 import java.util.ArrayList;
 import java.util.List;
 
+import me.iatog.characterdialogue.util.TextUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.core.Logger;
 import me.iatog.characterdialogue.CharacterDialoguePlugin;
@@ -30,14 +31,12 @@ public class PluginLoader implements Loader {
 				new DialogLoader(main)
 				);
 		
-		main.getLogger().info("§a"+main.getDescription().getName()+" enabled. §7"+main.getDescription().getVersion());
+		main.getLogger().info(TextUtils.colorize("&a"+main.getDescription().getName()+" enabled. &7"+main.getDescription().getVersion()));
 	}
 	
 	@Override
 	public void unload() {
-		loaders.forEach(loader -> {
-			loader.unload();
-		});
+		loaders.forEach(Loader::unload);
 		loaders.clear();
 	}
 	
