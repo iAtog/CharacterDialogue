41b3ec44ff6057b7160698898aaddaaff2421c00
iAtog <atogmc58vani@gmail.com>
2024-11-20 18:36:16 -0400

    fix: slowness effect not removing on join

--- src/main/java/me/iatog/characterdialogue/listeners/PlayerJoinListener.java
+++ src/main/java/me/iatog/characterdialogue/listeners/PlayerJoinListener.java
@@ -9,10 +9,12 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 import me.iatog.characterdialogue.CharacterDialoguePlugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 public class PlayerJoinListener implements Listener {
 	
-	private CharacterDialoguePlugin main;
+	private final CharacterDialoguePlugin main;
 	
 	public PlayerJoinListener(CharacterDialoguePlugin main) {
 		this.main = main;
@@ -23,12 +25,20 @@ public class PlayerJoinListener implements Listener {
 		Player player = event.getPlayer();
 		List<UUID> cache = main.getCache().getFrozenPlayers();
 
-		if(cache.contains(player.getUniqueId())) {
-			cache.remove(player.getUniqueId());
-		}
+        cache.remove(player.getUniqueId());
 		
 		if(main.getApi().canEnableMovement(player)) {
 			main.getApi().enableMovement(player);
+		}
+
+		PotionEffectType slowness = PotionEffectType.SLOW;
+
+		if(player.hasPotionEffect(slowness)) {
+			PotionEffect effect = player.getPotionEffect(slowness);
+
+			if(effect != null && effect.getAmplifier() == 4) {
+				player.removePotionEffect(slowness);
+			}
 		}
 	}
 	
