2b33d0cc673235efb50318b49b8426cb29f9737f
iAtog <atogmc58vani@gmail.com>
2024-11-23 19:21:35 -0400

    feat: added send centered message method

--- src/main/java/me/iatog/characterdialogue/util/TextUtils.java
+++ src/main/java/me/iatog/characterdialogue/util/TextUtils.java
@@ -2,6 +2,7 @@ package me.iatog.characterdialogue.util;
 
 import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -46,5 +47,44 @@ public class TextUtils {
 
         lines.add(currentLine.toString());
         return lines;
+    }
+
+    private final static int CENTER_PX = 154;
+
+    public static void sendCenteredMessage(Player player, String message){
+        if(message == null || message.equals("")) player.sendMessage("");
+        message = TextUtils.colorize(message);
+
+        int messagePxSize = 0;
+        boolean previousCode = false;
+        boolean isBold = false;
+
+        for(char c : message.toCharArray()){
+            if(c == '§'){
+                previousCode = true;
+                continue;
+            }else if(previousCode){
+                previousCode = false;
+                if(c == 'l' || c == 'L'){
+                    isBold = true;
+                    continue;
+                }else isBold = false;
+            }else{
+                DefaultFontInfo dFI = DefaultFontInfo.getDefaultFontInfo(c);
+                messagePxSize += isBold ? dFI.getBoldLength() : dFI.getLength();
+                messagePxSize++;
+            }
+        }
+
+        int halvedMessageSize = messagePxSize / 2;
+        int toCompensate = CENTER_PX - halvedMessageSize;
+        int spaceLength = DefaultFontInfo.SPACE.getLength() + 1;
+        int compensated = 0;
+        StringBuilder sb = new StringBuilder();
+        while(compensated < toCompensate){
+            sb.append(" ");
+            compensated += spaceLength;
+        }
+        player.sendMessage(sb.toString() + message);
     }
 }
