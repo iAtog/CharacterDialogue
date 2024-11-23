297d5fae88bc2064228a21e6bb0abb5bc22f966d
iAtog <atogmc58vani@gmail.com>
2024-11-23 01:49:54 -0400

    feat: created dialogue subcommand

---
+++ src/main/java/me/iatog/characterdialogue/command/DialogueCommands.java
@@ -0,0 +1,41 @@
+package me.iatog.characterdialogue.command;
+
+import me.fixeddev.commandflow.annotated.CommandClass;
+import me.fixeddev.commandflow.annotated.annotation.Command;
+import me.fixeddev.commandflow.annotated.annotation.OptArg;
+import me.fixeddev.commandflow.bukkit.annotation.Sender;
+import me.iatog.characterdialogue.api.dialog.Dialogue;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.Optional;
+
+@Command(names = "dialogue", desc = "Blah")
+public class DialogueCommands implements CommandClass {
+    @Command(names = "run", desc = "Run dialogue")
+    public void dialoguesCommand(@Sender CommandSender sender, @OptArg Optional<Dialogue> dialogueOpt, @OptArg Optional<Player> playerOpt) {
+        Player target;
+        if(!dialogueOpt.isPresent()) {
+            sender.sendMessage("No encontrado");
+            return;
+        }
+        Dialogue dialogue = dialogueOpt.get();
+
+        if(!playerOpt.isPresent()) {
+            if(!(sender instanceof Player)) {
+                sender.sendMessage("You arent a player");
+                return;
+            }
+            target = (Player) sender;
+        } else {
+            if(!playerOpt.get().isOnline()) {
+                sender.sendMessage("offlain");
+                return;
+            }
+            target = playerOpt.get();
+        }
+
+        sender.sendMessage(dialogue.getDisplayName() + ": test - " + target.getName());
+    }
+
+}
