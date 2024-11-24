2ce91020cf2fb3e2592d680dc6f7cb75c5eec764
iAtog <atogmc58vani@gmail.com>
2024-11-23 20:09:50 -0400

    chore: condition placeholders for more possibilities

---
+++ src/main/java/me/iatog/characterdialogue/placeholders/ConditionPlaceholder.java
@@ -0,0 +1,9 @@
+package me.iatog.characterdialogue.placeholders;
+
+import org.bukkit.entity.Player;
+
+// Purpose: add custom placeholders to check for certain very specific things
+public interface ConditionPlaceholder {
+    String getValue(Player player);
+    String getPlaceholderName();
+}
