0fbc7acfde725f6bb7f3694b1fd5c1a4aa308ba1
iAtog <atogmc58vani@gmail.com>
2024-11-24 22:09:21 -0400

    refactor: player may be null

--- src/main/java/me/iatog/characterdialogue/api/events/DialogueFinishEvent.java
+++ src/main/java/me/iatog/characterdialogue/api/events/DialogueFinishEvent.java
@@ -2,22 +2,28 @@ package me.iatog.characterdialogue.api.events;
 
 import me.iatog.characterdialogue.session.DialogSession;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.player.PlayerEvent;
 
-public class DialogueFinishEvent extends PlayerEvent {
+public class DialogueFinishEvent extends Event {
 
     private static final HandlerList handlers = new HandlerList();
 
     private final DialogSession session;
+    private final Player player;
 
     public DialogueFinishEvent(Player player, DialogSession session) {
-        super(player);
+        this.player = player;
         this.session = session;
     }
 
     public DialogSession getSession() {
         return session;
+    }
+
+    public Player getPlayer() {
+        return player;
     }
 
     public static HandlerList getHandlerList(){
