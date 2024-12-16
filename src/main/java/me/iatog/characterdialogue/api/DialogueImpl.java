f1a98c911011fa89bbb6f0fcc320ca4597af4969
iAtog <atogmc58vani@gmail.com>
2024-12-15 22:29:31 -0400

    feat: grant access to yaml file in dialogue

--- src/main/java/me/iatog/characterdialogue/api/DialogueImpl.java
+++ src/main/java/me/iatog/characterdialogue/api/DialogueImpl.java
@@ -14,140 +14,146 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DialogueImpl implements Dialogue {
-	
-	private String dialogName;
-	private List<String> lines;
-	private ClickType clickType;
-	private String displayName;
-	private DialogHologram hologram;
-	private List<String> firstInteraction;
-	private DialoguePermission permissions;
-	private boolean movement;
-	
-	private CharacterDialoguePlugin main;
-	
-	public DialogueImpl(CharacterDialoguePlugin instance, String dialogName, YamlDocument dialogsFile) {
-		Section section = dialogsFile.getSection("dialogue." + dialogName);
-		
-		if(!dialogsFile.contains("dialogue." + dialogName)) {
-			instance.getLogger().severe("No dialogue named \"" + dialogName + "\" was found.");
-			return;
-		}
-		
-		String click = section.getString("click", "RIGHT").toUpperCase();
-		
-		this.lines = section.getStringList("dialog");
-		this.clickType = ClickType.match(click) ? ClickType.valueOf(click) : ClickType.RIGHT;
-		this.firstInteraction = section.getStringList("first-interaction");
-		this.displayName = section.getString("display-name", "John the NPC");
-		this.dialogName = dialogName;
-		
-		if(section.getBoolean("hologram.enabled", false)) {
-			boolean enabled = section.getBoolean("hologram.enabled");
-			float yPosition = Float.parseFloat(section.getString("hologram.y-position", "0.4"));
-			List<String> hologramLines = section.getStringList("hologram.lines");
-			this.hologram = new DialogHologramImpl(enabled, yPosition, hologramLines);
-		}
-		
-		if(section.contains("permission")) {
-			if(section.isString("permission")) {
-				this.permissions = new DialoguePermission(section.getString("permission"), null);
-			} else {
-				this.permissions = new DialoguePermission(section.getString("permission.value"), section.getString("permission.message"));
-			}
-		}
-		
-		this.movement = section.getBoolean("allow-movement", true);
-		
-		this.main = instance;
-	}
-	
-	//public DialogueImpl(String dialogName) { this(CharacterDialoguePlugin.getInstance(), dialogName); }
-	
-	@Override
-	public String getName() {
-		return dialogName;
-	}
-	
-	@Override
-	public List<String> getLines() {
-		return lines;
-	}
 
-	@Override
-	public ClickType getClickType() {
-		return clickType;
-	}
+    private YamlDocument document;
 
-	@Override
-	public String getDisplayName() {
-		return displayName;
-	}
+    private String dialogName;
+    private List<String> lines;
+    private ClickType clickType;
+    private String displayName;
+    private DialogHologram hologram;
+    private List<String> firstInteraction;
+    private DialoguePermission permissions;
+    private boolean movement;
 
-	@Override
-	public DialogHologram getHologram() {
-		return hologram;
-	}
+    private CharacterDialoguePlugin main;
 
-	@Override
-	public List<String> getFirstInteractionLines() {
-		return firstInteraction;
-	}
+    public DialogueImpl(CharacterDialoguePlugin instance, String dialogName, YamlDocument dialogsFile) {
+        Section section = dialogsFile.getSection("dialogue." + dialogName);
 
-	@Override
-	public boolean isFirstInteractionEnabled() {
-		if (firstInteraction != null) {
-            return !firstInteraction.isEmpty();
+        if (! dialogsFile.contains("dialogue." + dialogName)) {
+            instance.getLogger().severe("No dialogue named \"" + dialogName + "\" was found.");
+            return;
+        }
+        this.document = dialogsFile;
+        String click = section.getString("click", "RIGHT").toUpperCase();
+
+        this.lines = section.getStringList("dialog");
+        this.clickType = ClickType.match(click) ? ClickType.valueOf(click) : ClickType.RIGHT;
+        this.firstInteraction = section.getStringList("first-interaction");
+        this.displayName = section.getString("display-name", "John the NPC");
+        this.dialogName = dialogName;
+
+        if (section.getBoolean("hologram.enabled", false)) {
+            boolean enabled = section.getBoolean("hologram.enabled");
+            float yPosition = Float.parseFloat(section.getString("hologram.y-position", "0.4"));
+            List<String> hologramLines = section.getStringList("hologram.lines");
+            this.hologram = new DialogHologramImpl(enabled, yPosition, hologramLines);
         }
 
-		return false;
-	}
+        if (section.contains("permission")) {
+            if (section.isString("permission")) {
+                this.permissions = new DialoguePermission(section.getString("permission"), null);
+            } else {
+                this.permissions = new DialoguePermission(section.getString("permission.value"), section.getString("permission.message"));
+            }
+        }
 
-	@Override
-	public boolean start(Player player, boolean debug, NPC npc) {
-		return runDialogue(player, debug, npc);
-	}
+        this.movement = section.getBoolean("allow-movement", true);
 
-	@Override
-	public boolean startFirstInteraction(Player player, boolean log, NPC npc) {
-		if(log) {
-			YamlDocument playerCache = main.getFileFactory().getPlayerCache();
-			List<String> readedDialogues = playerCache.getStringList("players." + player.getUniqueId() + ".readed-dialogues");
-			
-			if(!playerCache.contains("players." + player.getUniqueId())) {
-				readedDialogues = new ArrayList<>();
-			}
-			
-			if(readedDialogues.contains(getName())) {
-				return false;
-			}
-			
-			readedDialogues.add(getName());
-			playerCache.set("players." + player.getUniqueId() + ".readed-dialogues", readedDialogues);
+        this.main = instance;
+    }
+
+    //public DialogueImpl(String dialogName) { this(CharacterDialoguePlugin.getInstance(), dialogName); }
+
+    @Override
+    public String getName() {
+        return dialogName;
+    }
+
+    @Override
+    public List<String> getLines() {
+        return lines;
+    }
+
+    @Override
+    public ClickType getClickType() {
+        return clickType;
+    }
+
+    @Override
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    @Override
+    public DialogHologram getHologram() {
+        return hologram;
+    }
+
+    @Override
+    public List<String> getFirstInteractionLines() {
+        return firstInteraction;
+    }
+
+    @Override
+    public boolean isFirstInteractionEnabled() {
+        if (firstInteraction != null) {
+            return ! firstInteraction.isEmpty();
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean start(Player player, boolean debug, NPC npc) {
+        return runDialogue(player, debug, npc);
+    }
+
+    @Override
+    public boolean startFirstInteraction(Player player, boolean log, NPC npc) {
+        if (log) {
+            YamlDocument playerCache = main.getFileFactory().getPlayerCache();
+            List<String> readedDialogues = playerCache.getStringList("players." + player.getUniqueId() + ".readed-dialogues");
+
+            if (! playerCache.contains("players." + player.getUniqueId())) {
+                readedDialogues = new ArrayList<>();
+            }
+
+            if (readedDialogues.contains(getName())) {
+                return false;
+            }
+
+            readedDialogues.add(getName());
+            playerCache.set("players." + player.getUniqueId() + ".readed-dialogues", readedDialogues);
             try {
                 playerCache.save();
             } catch (IOException e) {
                 this.main.getLogger().severe("Error saving player cache for " + player.getName() + "'s");
             }
         }
-		
-		main.getApi().runDialogueExpressions(player, firstInteraction, displayName, this.dialogName);
-		return true;
-	}
-	
-	private boolean runDialogue(Player player, boolean debug, NPC npc) {
-		main.getApi().runDialogue(player, this, debug, npc);
-		return true;
-	}
 
-	@Override
-	public DialoguePermission getPermissions() {
-		return permissions;
-	}
+        main.getApi().runDialogueExpressions(player, firstInteraction, displayName, this.dialogName);
+        return true;
+    }
 
-	@Override
-	public boolean isMovementAllowed() {
-		return movement;
-	}
-	
+    private boolean runDialogue(Player player, boolean debug, NPC npc) {
+        main.getApi().runDialogue(player, this, debug, npc);
+        return true;
+    }
+
+    @Override
+    public DialoguePermission getPermissions() {
+        return permissions;
+    }
+
+    @Override
+    public boolean isMovementAllowed() {
+        return movement;
+    }
+
+    @Override
+    public YamlDocument getDocument() {
+        return document;
+    }
 }
