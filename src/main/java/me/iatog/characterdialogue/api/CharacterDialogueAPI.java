4b624349eb8cde9478c2359f6a9cd7693db772a9
iAtog <atogmc58vani@gmail.com>
2024-12-15 22:08:05 -0400

    refactor: fix spacing

--- src/main/java/me/iatog/characterdialogue/api/CharacterDialogueAPI.java
+++ src/main/java/me/iatog/characterdialogue/api/CharacterDialogueAPI.java
@@ -13,39 +13,55 @@ import org.bukkit.entity.Player;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 public interface CharacterDialogueAPI {
 
-	Dialogue getDialogue(String name);
+    public static CharacterDialogueAPI get() {
+        return new ApiImplementation(CharacterDialoguePlugin.getInstance());
+    }
 
-	void reloadHolograms();
+    Dialogue getDialogue(String name);
 
-	void loadHologram(int npcId);
-	
-	boolean readDialogBy(Player player, String dialog) throws IOException;
-	boolean wasReadedBy(Player player, String dialog);
-	
-	boolean readDialogBy(Player player, Dialogue dialog);
-	boolean wasReadedBy(Player player, Dialogue dialog);
-	
-	void runDialogue(Player player, Dialogue dialogue, boolean debugMode, NPC npc);
-	void runDialogue(Player player, String dialogueName, boolean debugMode, NPC npc);
-	void runDialogueExpression(Player player, String dialog);
-	void runDialogueExpression(Player player, String dialog, String npcName);
-	void runDialogueExpression(Player player, String dialog, String npcName, SingleUseConsumer<CompletedType> onComplete, DialogSession session, NPC npc);
-	void runDialogueExpressions(Player player, List<String> lines, ClickType type, NPC npc, String displayName, String dialogueName);
-	void runDialogueExpressions(Player player, List<String> lines, String displayName, String dialogueName) ;
-	
-	Dialogue getNPCDialogue(int id);
-	Map<String, Dialogue> getDialogues();
-	String getNPCDialogueName(int id);
-	int getBukkitVersion();
-	
-	boolean enableMovement(Player player);
-	boolean disableMovement(Player player);
-	boolean canEnableMovement(Player player);
-	
-	public static CharacterDialogueAPI get() {
-		return new ApiImplementation(CharacterDialoguePlugin.getInstance());
-	}
+    void reloadHolograms();
+
+    void loadHologram(int npcId);
+
+    boolean readDialogBy(Player player, String dialog) throws IOException;
+
+    boolean wasReadedBy(Player player, String dialog);
+
+    boolean readDialogBy(Player player, Dialogue dialog);
+
+    boolean wasReadedBy(Player player, Dialogue dialog);
+
+    void runDialogue(Player player, Dialogue dialogue, boolean debugMode, NPC npc);
+
+    void runDialogue(Player player, String dialogueName, boolean debugMode, NPC npc);
+
+    void runDialogueExpression(Player player, String dialog);
+
+    void runDialogueExpression(Player player, String dialog, String npcName);
+
+    void runDialogueExpression(Player player, String dialog, String npcName, SingleUseConsumer<CompletedType> onComplete, DialogSession session, NPC npc);
+
+    void runDialogueExpressions(Player player, List<String> lines, ClickType type, NPC npc, String displayName, String dialogueName);
+
+    void runDialogueExpressions(Player player, List<String> lines, String displayName, String dialogueName);
+
+    Dialogue getNPCDialogue(int id);
+
+    Map<String, Dialogue> getDialogues();
+
+    String getNPCDialogueName(int id);
+
+    int getBukkitVersion();
+
+    boolean enableMovement(Player player);
+
+    boolean disableMovement(Player player);
+
+    boolean canEnableMovement(Player player);
+
+    Pattern getLineRegex();
 }
