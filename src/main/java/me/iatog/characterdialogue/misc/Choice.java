c3b4a91a8aa3ff0559e6a6ee9a05e1188bdb6561
iAtog <atogmc58vani@gmail.com>
2024-12-17 03:38:02 -0400

    refactor: use record to reduce code

--- src/main/java/me/iatog/characterdialogue/misc/Choice.java
+++ src/main/java/me/iatog/characterdialogue/misc/Choice.java
@@ -2,34 +2,4 @@ package me.iatog.characterdialogue.misc;
 
 import me.iatog.characterdialogue.dialogs.DialogChoice;
 
-public class Choice {
-
-    private final int index;
-    private final String message;
-    private final Class<? extends DialogChoice> clazz;
-    private final String argument;
-
-    public Choice(int index, String message, Class<? extends DialogChoice> clazz, String argument) {
-        this.index = index;
-        this.message = message;
-        this.clazz = clazz;
-        this.argument = argument;
-    }
-
-    public int getIndex() {
-        return index;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public String getArgument() {
-        return argument;
-    }
-
-    public Class<? extends DialogChoice> getChoiceClass() {
-        return clazz;
-    }
-
-}
+public record Choice(int getIndex, String getMessage, Class<? extends DialogChoice> getChoiceClass, String getArgument) { }
