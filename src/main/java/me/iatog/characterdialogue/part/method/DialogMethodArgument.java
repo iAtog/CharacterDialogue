355b8b4d8f8dd491fdb78589dd59f026616f4630
iAtog <atogmc58vani@gmail.com>
2024-12-19 02:45:05 -0400

    refactor: use java record

--- src/main/java/me/iatog/characterdialogue/part/method/DialogMethodArgument.java
+++ src/main/java/me/iatog/characterdialogue/part/method/DialogMethodArgument.java
@@ -3,20 +3,6 @@ package me.iatog.characterdialogue.part.method;
 import me.iatog.characterdialogue.dialogs.DialogMethod;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class DialogMethodArgument {
-    private final String name;
-    private final DialogMethod<? extends JavaPlugin> method;
+public record DialogMethodArgument(String getName, DialogMethod<? extends JavaPlugin> getMethod) {
 
-    public DialogMethodArgument(String name, DialogMethod<? extends JavaPlugin> method) {
-        this.name = name;
-        this.method = method;
-    }
-
-    public DialogMethod<? extends JavaPlugin> getMethod() {
-        return method;
-    }
-
-    public String getName() {
-        return name;
-    }
 }
